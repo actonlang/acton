1deef6ae7d9e578c936d88b75bdda631a240b682
André Jonsson <andre.jonsson@dataductus.se>
2019-02-19 14:54:03 +0100

    added unit test example
    uses Unity (www.throwtheswitch.org/unity)
      it's included as a sub-project, could be proven to be suboptimal

---
+++ test/test_example/test_example.c
@@ -0,0 +1,20 @@
+#include "kernelops.h"
+#include "unity.h"
+
+
+void setUp() {
+    printf(">>>  setUp()\n");
+}
+
+void tearDown() {
+    printf("<<<  tearDown()\n");
+}
+
+void test_Test1() {
+    TEST_ASSERT_TRUE(1);
+}
+
+void test_WillFail() {
+    TEST_ASSERT_TRUE(0);
+}
+
