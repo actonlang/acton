5718ef31f0d8be9d371f48abffe3a1f744e1dafc
Andrei Agapi <andr.agapi@gmail.com>
2019-10-07 15:31:17 +0200

    Partial txn implementation 2

--- backend/txns.h
+++ backend/txns.h
@@ -49,9 +49,20 @@ typedef struct txn_read
 	WORD* col_keys;
 	int no_col_keys;
 
+	// For idx queries:
+
+	int idx_idx;
+
 	short query_type;
 
-	db_row_t* results;
+	// For non-range queries:
+
+	db_row_t* result;
+
+	// For range queries:
+
+	snode_t* start_row;
+	snode_t* end_row;
 } txn_read;
 
 typedef struct txn_state
@@ -94,6 +105,12 @@ int txn_read_cmp(WORD e1, WORD e2)
 	if(tr1->table_key != tr2->table_key)
 		return (int) ((long) tr1->table_key - (long) tr2->table_key);
 
+	if(tr1->query_type == QUERY_TYPE_READ_INDEX || tr1->query_type == QUERY_TYPE_READ_INDEX_RANGE)
+	{
+		if(tr1->idx_idx != tr2->idx_idx)
+			return tr1->idx_idx - tr2->idx_idx;
+	}
+
 	if(tr1->no_primary_keys != tr2->no_primary_keys)
 		return tr1->no_primary_keys - tr2->no_primary_keys;
 	for(int i=0;i<tr1->no_primary_keys;i++)
@@ -102,7 +119,7 @@ int txn_read_cmp(WORD e1, WORD e2)
 			return tr1->start_primary_keys[i] - tr2->start_primary_keys[i];
 	}
 
-	if(tr1->query_type == QUERY_TYPE_READ_ROW_RANGE)
+	if(tr1->query_type == QUERY_TYPE_READ_ROW_RANGE || tr1->query_type == QUERY_TYPE_READ_INDEX_RANGE)
 	{
 		for(int i=0;i<tr1->no_primary_keys;i++)
 		{
@@ -175,7 +192,8 @@ txn_read * get_txn_read(short query_type,
 						WORD* start_primary_keys, WORD* end_primary_keys, int no_primary_keys,
 						WORD* start_clustering_keys, WORD* end_clustering_keys, int no_clustering_keys,
 						WORD* col_keys, int no_col_keys,
-						db_row_t* results,
+						int idx_idx,
+						db_row_t* result, snode_t* start_row, snode_t* end_row,
 						WORD table_key)
 {
 	int total_col_count = no_primary_keys + no_clustering_keys;
@@ -231,7 +249,13 @@ txn_read * get_txn_read(short query_type,
 		offset += tr->no_col_keys * sizeof(WORD);
 	}
 
-	tr->results = results;
+	if(query_type == QUERY_TYPE_READ_INDEX || query_type == QUERY_TYPE_READ_INDEX_RANGE)
+		assert(idx_idx == -1);
+	tr->idx_idx = idx_idx;
+
+	tr->result = result;
+	tr->start_row = start_row;
+	tr->end_row = end_row;
 
 	return tr;
 }
@@ -240,51 +264,63 @@ int add_write_to_txn(short query_type, WORD * column_values, int no_cols, WORD t
 {
 	assert((query_type == QUERY_TYPE_UPDATE) || (query_type == QUERY_TYPE_DELETE));
 	txn_write * tw = get_txn_write(query_type, column_values, no_cols, table_key);
-	return skiplist_insert(ts->write_set, (WORD) tw, (WORD) tw, fastrandstate); // Note that these will overwrite previous values written for the variable in the same txn
+	return skiplist_insert(ts->write_set, (WORD) tw, (WORD) tw, fastrandstate); // Note that this will overwrite previous values written for the variable in the same txn (last write wins)
 }
 
 int add_row_read_to_txn(WORD* primary_keys, int no_primary_keys,
-						WORD table_key, db_row_t* results,
+						WORD table_key, db_row_t* result,
 						txn_state * ts, unsigned int * fastrandstate)
 {
-
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_ROW, primary_keys, NULL, no_primary_keys, NULL, NULL, 0, NULL, 0, -1, result, NULL, NULL, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
 }
 
 int add_row_range_read_to_txn(WORD* start_primary_keys, WORD* end_primary_keys, int no_primary_keys, WORD table_key,
 								snode_t* start_row, snode_t* end_row,
-								txn_state * ts, unsigned int * fastrandstate);
+								txn_state * ts, unsigned int * fastrandstate)
+{
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_ROW_RANGE, start_primary_keys, end_primary_keys, no_primary_keys, NULL, NULL, 0, NULL, 0, -1, NULL, start_row, end_row, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
+}
+
 int add_cell_read_to_txn(WORD* primary_keys, int no_primary_keys, WORD* clustering_keys, int no_clustering_keys,
-								WORD table_key, db_row_t* results,
-								txn_state * ts, unsigned int * fastrandstate);
+								WORD table_key, db_row_t* result,
+								txn_state * ts, unsigned int * fastrandstate)
+{
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_CELL, primary_keys, NULL, no_primary_keys, clustering_keys, NULL, no_clustering_keys, NULL, 0, -1, result, NULL, NULL, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
+}
+
 int add_cell_range_read_to_txn(WORD* primary_keys, int no_primary_keys, WORD* start_clustering_keys,
 								WORD* end_clustering_keys, int no_clustering_keys,
 								WORD table_key,
 								snode_t* start_row, snode_t* end_row,
-								txn_state * ts, unsigned int * fastrandstate);
-int add_col_read_to_txn(WORD* primary_keys, int no_primary_keys, WORD* clustering_keys, int no_clustering_keys,
-								WORD* col_keys, int no_columns,
-								WORD table_key, WORD* results,
-								txn_state * ts, unsigned int * fastrandstate);
-db_row_t* add_index_read_to_txn(WORD index_key, int idx_idx, WORD table_key, txn_state * ts, unsigned int * fastrandstate);
-int add_index_range_read_to_txn(int idx_idx, WORD start_idx_key, WORD end_idx_key, snode_t** start_row, snode_t** end_row, WORD table_key, txn_state * ts, unsigned int * fastrandstate);
-
-
-WORD* start_primary_keys;
-WORD* end_primary_keys;
-int no_primary_keys;
-
-WORD* start_clustering_keys;
-WORD* end_clustering_keys;
-int no_clustering_keys;
-
-short query_type;
-
-
+								txn_state * ts, unsigned int * fastrandstate)
 {
-	txn_write * tw = get_txn_write(column_values, no_cols, table_key);
-	return skiplist_insert(ts->write_set, (WORD) tw, (WORD) tw, fastrandstate); // Note that these will overwrite previous values written for the variable in the same txn
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_CELL_RANGE, primary_keys, NULL, no_primary_keys, start_clustering_keys, end_clustering_keys, no_clustering_keys, NULL, 0, -1, NULL, start_row, end_row, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
 }
 
+int add_col_read_to_txn(WORD* primary_keys, int no_primary_keys, WORD* clustering_keys, int no_clustering_keys,
+								WORD* col_keys, int no_columns,
+								WORD table_key, db_row_t* result,
+								txn_state * ts, unsigned int * fastrandstate)
+{
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_COLS, primary_keys, NULL, no_primary_keys, clustering_keys, NULL, no_clustering_keys, col_keys, no_columns, -1, result, NULL, NULL, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
+}
+
+int add_index_read_to_txn(WORD* index_key, int idx_idx, WORD table_key, db_row_t* result, txn_state * ts, unsigned int * fastrandstate)
+{
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_INDEX, index_key, NULL, 1, NULL, NULL, 0, NULL, 0, idx_idx, result, NULL, NULL, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
+}
+
+int add_index_range_read_to_txn(int idx_idx, WORD* start_idx_key, WORD* end_idx_key, snode_t* start_row, snode_t* end_row, WORD table_key, txn_state * ts, unsigned int * fastrandstate)
+{
+	txn_read * tr = get_txn_read(QUERY_TYPE_READ_INDEX_RANGE, start_idx_key, end_idx_key, 1, NULL, NULL, 0, NULL, 0, idx_idx, NULL, start_row, end_row, table_key);
+	return skiplist_insert(ts->read_set, (WORD) tr, (WORD) tr, fastrandstate); // Note that this will overwrite previous values read for the variable in the same txn (last read wins)
+}
 
 
 #endif /* BACKEND_TXNS_H_ */
