0282920a8986288fa51ee4e3a3c0f05906468833
Johan Nordlander <johan.nordlander@dataductus.se>
2019-11-27 23:16:35 +0100

    Added affinity control functions (for macos).

--- rts/kernel.c
+++ rts/kernel.c
@@ -1,9 +1,62 @@
-//#define _POSIX_C_SOURCE 200112L  // clock_nanosleep()//
 #include <time.h>
 #include <math.h> // round()
 #include <stdatomic.h>
 #include <unistd.h>  // sysconf()
 #include <pthread.h>
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <mach/mach_init.h>
+#include <mach/thread_policy.h>
+
+#define SYSCTL_CORE_COUNT   "machdep.cpu.core_count"
+
+typedef struct cpu_set {
+  uint32_t    count;
+} cpu_set_t;
+
+static inline int
+CPU_ISSET(int num, cpu_set_t *cs) { return (cs->count & (1 << num)); }
+
+int sched_getaffinity(pid_t pid, size_t cpu_size, cpu_set_t *cpu_set)
+{
+  int32_t core_count = 0;
+  size_t  len = sizeof(core_count);
+  int ret = sysctlbyname(SYSCTL_CORE_COUNT, &core_count, &len, 0, 0);
+  if (ret) {
+    printf("error while get core count %d\n", ret);
+    return -1;
+  }
+  cpu_set->count = 0;
+  for (int i = 0; i < core_count; i++) {
+    cpu_set->count |= (1 << i);
+  }
+
+  return 0;
+}
+
+kern_return_t thread_policy_set(
+					thread_t thread,
+					thread_policy_flavor_t flavor,
+					thread_policy_t policy_info,
+					mach_msg_type_number_t count);
+
+int pthread_setaffinity_np(pthread_t thread, size_t cpu_size, cpu_set_t *cpu_set) {
+    int core = 0;
+    for (core = 0; core < 8 * cpu_size; core++) {
+        if (CPU_ISSET(core, cpu_set))
+            break;
+    }
+    thread_affinity_policy_data_t policy = { core };
+
+    thread_port_t mach_thread = pthread_mach_thread_np(thread);
+    thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY, (thread_policy_t)&policy, 1);
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////
 
 #include "kernelops.h"
 
