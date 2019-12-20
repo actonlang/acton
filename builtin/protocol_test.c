aa8bd909c1e8bc32db6ca03518fbbef0f9316246
Björn von Sydow <bjorn.von.sydow@gmail.com>
2019-12-20 21:20:09 +0100

    list implementation almost complete

---
+++ builtin/protocol_test.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "common.h"
+#include "list.h"
+
+void RAISE(exception e) {
+  fprintf(stderr,"exception raised\n");
+  exit(1);
+}
+
+long fromWord($WORD w) {
+  return *(long*) w;
+}
+
+$WORD toWord(long i) {
+  $WORD res = malloc(sizeof(long));
+  *(long*)res = i;
+  return res;
+}
+
+void printlist($list lst) {
+  $WORD w;
+  printf("[");
+  for (int i=0; i < *$list_len(lst)-1; i++) {
+    w = $list_getitem(lst,i);
+    printf("%ld, ",fromWord(w));
+  }
+  if (*$list_len(lst) > 0) {
+    w = $list_getitem(lst,*$list_len(lst)-1);
+    printf("%ld",fromWord(w));
+  }
+  printf("]\n");
+}
+
+$list fromto(int a, int b) {
+  $list res = $list_fromiter(NULL);
+  for (long i = a; i<b; i++)
+    $list_append(res,toWord(i));
+  return res;
+}
+// sum : [A (Plus)] => (Sequence[A]) -> A
+// Here we do the special case of a list of lists so the Plus ancestor works...
+$WORD sum(int n, Sequence s) {
+  $WORD res = s->__class__->Sliceable$__methods__->Indexed$__methods__->__getitem__((Indexed)s,toWord(0));
+  // We cannot follow the chain past Container_Eq, since we have a function there 
+  //  int len = s->__class__->Container_Eq$__methods__->Collection_$__methods__->__len__((Collection)s);
+  for (int i = 1; i < n; i++) {
+    $WORD nxt = s->__class__->Sliceable$__methods__->Indexed$__methods__->__getitem__((Indexed)s,toWord(i));
+    res = s->__class__->Plus$__methods__->__add__(res,nxt);
+  }
+  return res;
+}
+ 
+int main() {
+  list_instance_init();
+  $list lst = $list_fromiter(NULL);
+  for (int i = 1; i< 10; i++) {
+     $list_append(lst,fromto(i,2*i));
+  }
+  Sequence s = Sequence$__pack__(Sequence_$list_instance,($WORD)lst);
+  $list lst2 = ($list)sum(*$list_len(lst),s);
+  printlist(lst2);
+}
+                 
+  
---
+++ builtin/slice.c
@@ -0,0 +1,45 @@
+#include <stddef.h>
+#include "common.h"
+
+/* Normalize slice notation, so that
+- if step == 0, VALUEERROR is raised
+
+- Otherwise, 
+   - on input, len must be the # of elements in the sequence being sliced
+   - on output 
+       - 0 <= *start < len is the starting position
+       - 0 <= *stop < len is the ending position (*non-inclusive*!)
+       - *step is the step size
+       - *slen is the # of elements in the slice. 
+*/
+
+void normalize_slice(Slice slc, int len, int *slen, int *start, int *stop, int *step) {
+  if (slc->step == NULL)
+    *step = 1;
+  else
+    *step = *slc->step;
+  if (*step == 0) {
+    exception e;
+    MKEXCEPTION(e,VALUEERROR);
+    RAISE(e);
+  }
+  if (slc->start == NULL)
+    *start = *step > 0 ? 0 : len-1;
+  else {
+    *start = *slc->start;
+    *start = *start >=0 ? (*start < len  ? *start : len-1 + (*step > 0))
+                        : (*start > -len ? len+*start : 0);
+  }
+  if (slc->stop == NULL)
+    *stop = *step > 0 ? len : -1;
+  else {
+    *stop = *slc->stop;
+    *stop = *stop >= 0 ? (*stop < len  ? *stop : len-1 + (*step > 0)) 
+                       : (*stop > -len ? len+*stop : 0);
+  }
+  
+  if ((*step > 0 && *start >= *stop) || (*step < 0 && *start <= *stop))
+    *slen = 0;
+  else
+    *slen = (*stop-*start)/ *step + ((*stop-*start)%*step != 0);
+}
