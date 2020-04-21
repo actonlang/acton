3d3787e976ce090711fb405fc734df6b412ca071
Johan Nordlander <johan.nordlander@dataductus.se>
2020-04-21 17:32:10 +0200

    Changed Pingpong to use boxed integers (incomplete).

--- rts/pingpong.h
+++ rts/pingpong.h
@@ -14,57 +14,57 @@ typedef struct lambda$1 *lambda$1;
 typedef struct lambda$2 *lambda$2;
 typedef struct Pingpong *Pingpong;
 
-void lambda$1$__init__(lambda$1, Pingpong, int, int);
+void lambda$1$__init__(lambda$1, Pingpong, $int, $int);
 $R lambda$1$enter (lambda$1, $Cont);
 
-void lambda$2$__init__(lambda$2, Pingpong, int);
+void lambda$2$__init__(lambda$2, Pingpong, $int);
 $R lambda$2$enter (lambda$2, $Cont);
 
-$R Pingpong$__init__(Pingpong, int, $Cont);
-$R Pingpong$ping(Pingpong, int, $Cont);
-$R Pingpong$pong(Pingpong, int, int, $Cont);
+$R Pingpong$__init__(Pingpong, $int, $Cont);
+$R Pingpong$ping(Pingpong, $int, $Cont);
+$R Pingpong$pong(Pingpong, $int, $int, $Cont);
 
 struct lambda$1 {
     union {
-        struct lambda$1$class *__class__;
+        struct lambda$1$class *$class;
         struct $Cont$class super;
     };
     Pingpong self;
-    int count;
-    int q;
+    $int count;
+    $int q;
 };
 struct lambda$1$class {
     char *GCINFO;
-    void (*__init__)(lambda$1, Pingpong, int, int);
+    void (*__init__)(lambda$1, Pingpong, $int, $int);
     $R (*enter) (lambda$1, $Cont);
 };
 
 struct lambda$2 {
     union {
-        struct lambda$2$class *__class__;
+        struct lambda$2$class *$class;
         struct $Cont$class super;
     };
     Pingpong self;
-    int q;
+    $int q;
 };
 struct lambda$2$class {
     char *GCINFO;
-    void (*__init__)(lambda$2, Pingpong, int);
+    void (*__init__)(lambda$2, Pingpong, $int);
     $R (*enter) (lambda$2, $Cont);
 };
 
 struct Pingpong {
     union {
-        struct Pingpong$class *__class__;
+        struct Pingpong$class *$class;
         struct $Actor super;
     };
-    int count;
+    $int count;
 };
 struct Pingpong$class {
     char *GCINFO;
-    $R (*__init__)(Pingpong,int,$Cont);
-    $R (*ping)(Pingpong,int,$Cont);
-    $R (*pong)(Pingpong,int,int,$Cont);
+    $R (*__init__)(Pingpong,$int,$Cont);
+    $R (*ping)(Pingpong,$int,$Cont);
+    $R (*pong)(Pingpong,$int,$int,$Cont);
 };
 
 extern struct lambda$1$class lambda$1$methods;
--- rts/root.c
+++
@@ -1,5 +0,0 @@
-#include "pingpong.h"
-
-$R $ROOT(int env, $Cont then) {
-    return $NEWCC(Pingpong, then, env);
-}
