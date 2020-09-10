fa9a1a0a829b6c71cf9e6dc623e61d8dc388d83d
Björn von Sydow <bjorn.von.sydow@gmail.com>
2020-09-10 13:33:46 +0200

    minor improvements to numpy test programs

--- numpy/numpytests/test1_lists.py
+++ numpy/numpytests/test1_lists.py
@@ -1,9 +1,9 @@
 import sys
 
 n = int(sys.argv[1])
-
+iters =  int(sys.argv[2])
 x = [i/(n-1) for i in range(n)]
 
-for i in range(20):
+for i in range(iters):
     r = [[a + b for a in x] for b in x]
     print(sum([sum(a) for a in r]))
--- numpy/numpytests/test1_mutate.py
+++ numpy/numpytests/test1_mutate.py
@@ -2,10 +2,11 @@ import numpy as np
 import sys
 
 n = int(sys.argv[1])
+iters = int(sys.argv[2])
 
 x = np.linspace(0,1,n)
 
-for i in range(20):
+for i in range(iters):
     r = np.empty((n,n))
     
     for i in range(n):
