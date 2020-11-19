18663c6ea49a2386d3b4f691b8192f604fbb4955
Johan Nordlander <johan.nordlander@dataductus.se>
2020-11-19 21:00:42 +0100

    File cleanup.

--- compiler/sysRoot/hash.h
+++
@@ -1,9 +0,0 @@
-
-long $int_hash($int n);
-long $float_hash($float v);
-long $complex_hash($complex c);
-
-long $string_hash($str s);
-
-long $pointer_hash($WORD w);
-long $tuple_hash($Hashable$tuple wit,$tuple tup);
