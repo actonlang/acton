0247efd55fa0d0c9609f504d78a52cb01b2cb854
Björn von Sydow <bjorn.von.sydow@gmail.com>
2020-12-09 09:12:18 +0100

    added module math with protocol RealFuns and instance for float

---
+++ math/math.c
@@ -0,0 +1,162 @@
+#include "math/math.h"
+
+
+$NoneType math$$RealFuns$__init__ (math$$RealFuns w$self) {
+    return $None;
+}
+math$$RealFuns math$$RealFuns$new() {
+    math$$RealFuns $tmp = malloc(sizeof(struct math$$RealFuns));
+    $tmp->$class = &math$$RealFuns$methods;
+    math$$RealFuns$methods.__init__($tmp);
+    return $tmp;
+}
+struct math$$RealFuns$class math$$RealFuns$methods;
+$NoneType math$$RealFuns$float$__init__ (math$$RealFuns$float w$self) {
+    math$$RealFuns$methods.__init__((math$$RealFuns)w$self);
+    return $None;
+}
+
+$NoneType math$$RealFuns$float$__serialize__(math$$RealFuns$float wit, $Serial$state state) {
+    return $None;
+}
+
+math$$RealFuns$float math$$RealFuns$float$__deserialize__($Serial$state state) {
+    math$$RealFuns$float res = $DNEW(math$$RealFuns$float,state);
+    return res;
+}
+$float math$$RealFuns$float$sqrt(math$$RealFuns$float wit, $float x) {
+  return to$float(sqrt(x->val));
+}
+$float math$$RealFuns$float$exp(math$$RealFuns$float wit, $float x) {
+  return to$float(exp(x->val));
+}
+$float math$$RealFuns$float$log(math$$RealFuns$float wit, $float x) {
+  return to$float(log(x->val));
+}
+$float math$$RealFuns$float$sin(math$$RealFuns$float wit, $float x) {
+  return to$float(sin(x->val));
+}
+$float math$$RealFuns$float$cos(math$$RealFuns$float wit, $float x) {
+  return to$float(cos(x->val));
+}
+$float math$$RealFuns$float$tan(math$$RealFuns$float wit, $float x) {
+  return to$float(tan(x->val));
+}
+$float math$$RealFuns$float$asin(math$$RealFuns$float wit, $float x) {
+  return to$float(asin(x->val));
+}
+$float math$$RealFuns$float$acos(math$$RealFuns$float wit, $float x) {
+  return to$float(acos(x->val));
+}
+$float math$$RealFuns$float$atan(math$$RealFuns$float wit, $float x) {
+  return to$float(atan(x->val));
+}
+$float math$$RealFuns$float$sinh(math$$RealFuns$float wit, $float x) {
+  return to$float(sinh(x->val));
+}
+$float math$$RealFuns$float$cosh(math$$RealFuns$float wit, $float x) {
+  return to$float(cosh(x->val));
+}
+$float math$$RealFuns$float$tanh(math$$RealFuns$float wit, $float x) {
+  return to$float(tanh(x->val));
+}
+$float math$$RealFuns$float$asinh(math$$RealFuns$float wit, $float x) {
+  return to$float(asinh(x->val));
+}
+$float math$$RealFuns$float$acosh(math$$RealFuns$float wit, $float x) {
+  return to$float(acosh(x->val));
+}
+$float math$$RealFuns$float$atanh(math$$RealFuns$float wit, $float x) {
+  return to$float(atanh(x->val));
+}
+
+                      
+math$$RealFuns$float math$$RealFuns$float$new() {
+    math$$RealFuns$float $tmp = malloc(sizeof(struct math$$RealFuns$float));
+    $tmp->$class = &math$$RealFuns$float$methods;
+    math$$RealFuns$float$methods.__init__($tmp);
+    return $tmp;
+}
+struct math$$RealFuns$float$class math$$RealFuns$float$methods;
+int math$$done$ = 0;
+void math$$__init__ () {
+    if (math$$done$) return;
+    math$$done$ = 1;
+    {
+        math$$RealFuns$methods.$GCINFO = "math$$RealFuns";
+        math$$RealFuns$methods.$superclass = NULL;
+        math$$RealFuns$methods.__init__ = math$$RealFuns$__init__;
+        $register(&math$$RealFuns$methods);
+    }
+    {
+        math$$RealFuns$float$methods.$GCINFO = "math$$RealFuns$float";
+        math$$RealFuns$float$methods.$superclass = ($Super$class)&math$$RealFuns$methods;
+        math$$RealFuns$float$methods.__serialize__ = math$$RealFuns$float$__serialize__,
+        math$$RealFuns$float$methods.__deserialize__ = math$$RealFuns$float$__deserialize__,
+        math$$RealFuns$float$methods.__bool__ = ($bool (*)(math$$RealFuns$float))$default__bool__,
+        math$$RealFuns$float$methods.__str__ = ($str (*)(math$$RealFuns$float))$default__str__,
+        math$$RealFuns$float$methods.__init__ = math$$RealFuns$float$__init__;
+        math$$RealFuns$float$methods.sqrt = math$$RealFuns$float$sqrt;        
+        math$$RealFuns$float$methods.exp = math$$RealFuns$float$exp;        
+        math$$RealFuns$float$methods.log = math$$RealFuns$float$log;        
+        math$$RealFuns$float$methods.sin = math$$RealFuns$float$sin;        
+        math$$RealFuns$float$methods.cos = math$$RealFuns$float$cos;        
+        math$$RealFuns$float$methods.tan = math$$RealFuns$float$tan;        
+        math$$RealFuns$float$methods.asin = math$$RealFuns$float$asin;        
+        math$$RealFuns$float$methods.acos = math$$RealFuns$float$acos;        
+        math$$RealFuns$float$methods.atan = math$$RealFuns$float$atan;        
+        math$$RealFuns$float$methods.sinh = math$$RealFuns$float$sinh;        
+        math$$RealFuns$float$methods.cosh = math$$RealFuns$float$cosh;        
+        math$$RealFuns$float$methods.tanh = math$$RealFuns$float$tanh;        
+        math$$RealFuns$float$methods.asinh = math$$RealFuns$float$asinh;        
+        math$$RealFuns$float$methods.acosh = math$$RealFuns$float$acosh;        
+        math$$RealFuns$float$methods.atanh = math$$RealFuns$float$atanh;        
+        $register(&math$$RealFuns$float$methods);
+    }
+}
+
+$WORD math$$sqrt (math$$RealFuns wit, $WORD x) {
+  return wit->$class->sqrt(wit,x);
+}
+$WORD math$$exp (math$$RealFuns wit, $WORD x) {
+  return wit->$class->exp(wit,x);
+}
+$WORD math$$log (math$$RealFuns wit, $WORD x) {
+  return wit->$class->log(wit,x);
+}
+$WORD math$$sin (math$$RealFuns wit, $WORD x) {
+  return wit->$class->sin(wit,x);
+}
+$WORD math$$cos (math$$RealFuns wit, $WORD x) {
+  return wit->$class->cos(wit,x);
+}
+$WORD math$$tan (math$$RealFuns wit, $WORD x) {
+  return wit->$class->tan(wit,x);
+}
+$WORD math$$asin (math$$RealFuns wit, $WORD x) {
+  return wit->$class->asin(wit,x);
+}
+$WORD math$$acos (math$$RealFuns wit, $WORD x) {
+  return wit->$class->acos(wit,x);
+}
+$WORD math$$atan (math$$RealFuns wit, $WORD x) {
+  return wit->$class->atan(wit,x);
+}
+$WORD math$$sinh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->sinh(wit,x);
+}
+$WORD math$$cosh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->cosh(wit,x);
+}
+$WORD math$$tanh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->tanh(wit,x);
+}
+$WORD math$$asinh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->asinh(wit,x);
+}
+$WORD math$$acosh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->acosh(wit,x);
+}
+$WORD math$$atanh (math$$RealFuns wit, $WORD x) {
+  return wit->$class->atanh(wit,x);
+}
---
+++ math/math.h
@@ -0,0 +1,84 @@
+#pragma once
+#include "builtin/builtin.h"
+#include "builtin/minienv.h"
+#include "rts/rts.h"
+struct math$$RealFuns;
+typedef struct math$$RealFuns *math$$RealFuns;
+struct math$$RealFuns$class {
+    char *$GCINFO;
+    $int $class_id;
+    $Super$class $superclass;
+    $NoneType (*__init__) (math$$RealFuns);
+    $NoneType (*__serialize__) (math$$RealFuns, $Serial$state);
+    math$$RealFuns (*__deserialize__) ($Serial$state);
+    $bool (*__bool__)(math$$RealFuns);
+    $str (*__str__)(math$$RealFuns);
+    $WORD (*sqrt) (math$$RealFuns, $WORD);
+    $WORD (*exp) (math$$RealFuns, $WORD);
+    $WORD (*log) (math$$RealFuns, $WORD);
+    $WORD (*sin) (math$$RealFuns, $WORD);
+    $WORD (*cos) (math$$RealFuns, $WORD);
+    $WORD (*tan) (math$$RealFuns, $WORD);
+    $WORD (*asin) (math$$RealFuns, $WORD);
+    $WORD (*acos) (math$$RealFuns, $WORD);
+    $WORD (*atan) (math$$RealFuns, $WORD);
+    $WORD (*sinh) (math$$RealFuns, $WORD);
+    $WORD (*cosh) (math$$RealFuns, $WORD);
+    $WORD (*tanh) (math$$RealFuns, $WORD);
+    $WORD (*asinh) (math$$RealFuns, $WORD);
+    $WORD (*acosh) (math$$RealFuns, $WORD);
+    $WORD (*atanh) (math$$RealFuns, $WORD);
+};
+struct math$$RealFuns {
+    struct math$$RealFuns$class *$class;
+};
+extern struct math$$RealFuns$class math$$RealFuns$methods;
+math$$RealFuns math$$RealFuns$new();
+struct math$$RealFuns$float;
+typedef struct math$$RealFuns$float *math$$RealFuns$float;
+struct math$$RealFuns$float$class {
+    char *$GCINFO;
+    $int $class_id;
+    $Super$class $superclass;
+    $NoneType (*__init__) (math$$RealFuns$float);
+    $NoneType (*__serialize__) (math$$RealFuns$float, $Serial$state);
+    math$$RealFuns$float (*__deserialize__) ($Serial$state);
+    $bool (*__bool__)(math$$RealFuns$float);
+    $str (*__str__)(math$$RealFuns$float);
+    $float (*sqrt) (math$$RealFuns$float, $float);
+    $float (*exp) (math$$RealFuns$float, $float);
+    $float (*log) (math$$RealFuns$float, $float);
+    $float (*sin) (math$$RealFuns$float, $float);
+    $float (*cos) (math$$RealFuns$float, $float);
+    $float (*tan) (math$$RealFuns$float, $float);
+    $float (*asin) (math$$RealFuns$float, $float);
+    $float (*acos) (math$$RealFuns$float, $float);
+    $float (*atan) (math$$RealFuns$float, $float);
+    $float (*sinh) (math$$RealFuns$float, $float);
+    $float (*cosh) (math$$RealFuns$float, $float);
+    $float (*tanh) (math$$RealFuns$float, $float);
+    $float (*asinh) (math$$RealFuns$float, $float);
+    $float (*acosh) (math$$RealFuns$float, $float);
+    $float (*atanh) (math$$RealFuns$float, $float);
+};
+struct math$$RealFuns$float {
+    struct math$$RealFuns$float$class *$class;
+};
+extern struct math$$RealFuns$float$class math$$RealFuns$float$methods;
+math$$RealFuns$float math$$RealFuns$float$new();
+$WORD math$$sqrt (math$$RealFuns, $WORD);
+$WORD math$$exp (math$$RealFuns, $WORD);
+$WORD math$$log (math$$RealFuns, $WORD);
+$WORD math$$sin (math$$RealFuns, $WORD);
+$WORD math$$cos (math$$RealFuns, $WORD);
+$WORD math$$tan (math$$RealFuns, $WORD);
+$WORD math$$asin (math$$RealFuns, $WORD);
+$WORD math$$acos (math$$RealFuns, $WORD);
+$WORD math$$atan (math$$RealFuns, $WORD);
+$WORD math$$sinh (math$$RealFuns, $WORD);
+$WORD math$$cosh (math$$RealFuns, $WORD);
+$WORD math$$tanh (math$$RealFuns, $WORD);
+$WORD math$$asinh (math$$RealFuns, $WORD);
+$WORD math$$acosh (math$$RealFuns, $WORD);
+$WORD math$$atanh (math$$RealFuns, $WORD);
+void math$$__init__ ();
