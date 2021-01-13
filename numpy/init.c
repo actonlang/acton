6b9a97bedb1abc3eb739ae2e0326946896cd2834
Björn von Sydow <bjorn.von.sydow@gmail.com>
2021-01-13 17:10:36 +0100

    still another loess_smoother (with decomposition

--- numpy/init.c
+++ numpy/init.c
@@ -3,7 +3,7 @@ int numpy$$done = 0;
 void numpy$$__init__() {
     if (numpy$$done) return;
     numpy$$done = 1;
-    $register(&numpy$$ndarray$methods);
+    $register_force(ATOM_ID,&numpy$$ndarray$methods);
     $register(&numpy$$Iterator$ndarray$methods);
     $register(&numpy$$Primitive$int$methods);
     $register(&numpy$$Primitive$float$methods);
