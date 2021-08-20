34ef9bc357fe6cd185af2010ed834950252de30b
Kristian Larsson <kristian@spritelink.net>
2021-08-20 10:57:31 +0200

    Add acton.rts.sleep() function
    Which implies we are adding the acton.rts module too! There's a simple
    test case added.
    
    I don't know where to write documentation, like it would be nice to have
    a docstring under the function but there's no function defined in Acton!
    Just a function type signature, so no place to put the docstring from
    which we can later generate documentation. I added a big comment for
    now.
    
    As stated in this comment, acton.rts.sleep() is sort of bad and
    shouldn't normally be used, but it is deliberately implemented the way
    it is and can be useful for benchmarking scenarios and similar.

---
+++ modules/acton/rts.c
@@ -0,0 +1,12 @@
+#include "modules/acton/rts.h"
+#include <unistd.h>
+$NoneType acton$rts$$sleep ($float sleep_time) {
+    int to_sleep = sleep_time->val*1000000;
+    usleep(to_sleep);
+    return $None;
+}
+int acton$rts$$done$ = 0;
+void acton$rts$$__init__ () {
+    if (acton$rts$$done$) return;
+    acton$rts$$done$ = 1;
+}
