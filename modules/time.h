d4e737e63c60d9c32a8d40e9dbe52ba68818d00b
Kristian Larsson <kristian@spritelink.net>
2021-08-27 15:55:01 +0200

    Pythonify time module
    This changes the time.time() function to return a float, just as the
    Python time module does. However, we still return nano second precision
    instead of microsecond precision as in Python. In addition, we add a
    time.time_ns() function that returns the current time as nanoseconds,
    just like in Python.
    
    I've also added the equivalent to Pythons monotonic time functions,
    monotonic() and monotonic_ns().
    
    We no longer do a hard exit but return an exception in case we can't get
    the time.
    
    Tests have been revamped somewhat.

--- modules/time.h
+++ modules/time.h
@@ -2,5 +2,8 @@
 #include "builtin/builtin.h"
 #include "builtin/minienv.h"
 #include "rts/rts.h"
-$int time$$time ();
+$float time$$monotonic ();
+$int time$$monotonic_ns ();
+$float time$$time ();
+$int time$$time_ns ();
 void time$$__init__ ();
