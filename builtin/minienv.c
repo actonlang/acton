1dada97252f0ba04de86eee99459619d64c7b3cd
Johan Nordlander <johan.nordlander@dataductus.se>
2021-09-16 15:59:33 +0200

    Resorted to epoll_wait and millisecond timeouts to match linux kernels < 5.11.

--- builtin/minienv.c
+++ builtin/minienv.c
@@ -108,8 +108,10 @@ void EVENT_del_read(int fd) {
     epoll_ctl(ep, EPOLL_CTL_DEL, fd, &fd_data[fd].event_spec);
 }
 int EVENT_wait(EVENT_type *ev, struct timespec *timeout) {
+    int msec = timeout->tv_sec * 1000 + timeout->tv_nsec / 1000000;
     printf("## EPOLL kevent\n");
-    return epoll_pwait2(ep, ev, 1, timeout, NULL);
+    return epoll_wait(ep, ev, 1, msec);
+//    return epoll_pwait2(ep, ev, 1, timeout, NULL);        // appears in linux kernel 5.11
 }
 int EVENT_fd(EVENT_type *ev) {
     return ev->data.fd;
