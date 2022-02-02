c23ce365191bb1ea51bced5b1627aee5a41b9409
Kristian Larsson <kristian@spritelink.net>
2022-02-02 12:18:35 +0100

    Use unittest for DB tests
    Rather than use some home hacked stuff we just use the standard
    unittest. I think this looks much better, we get a prettier and more
    robust structure with setUp() & tearDown() etc.
    
    Also clear that the noquorum tests really should fail. RTS is doing the
    wrong thing.

--- test/test_db.py
+++ test/test_db.py
@@ -11,6 +11,7 @@ import socket
 import subprocess
 import sys
 import time
+import unittest
 
 ACTONDB="../dist/bin/actondb"
 BASEPORT=32001
@@ -20,60 +21,46 @@ def get_db_args(base_port, replication_factor):
     return [item for sublist in map(lambda x: ("--rts-ddb-host", x), [f"127.0.0.1:{base_port+idx}" for idx in range(replication_factor)]) for item in sublist]
 
 
-class MonSock:
-    """Communicate with the monitor interface of actondb or RTS using netstrings
-    encoded payloads
-    """
-    def __init__(self, address):
-        self.address = address
-        self.sock = None
-        self.buf = b""
+def mon_cmd(address, cmd, retries=5):
 
-    def connect(self):
-        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        try:
-            self.sock.connect(self.address)
-        except Exception as exc:
-            pass
-
-    def cmd(self, cmd, retries=5):
-        if self.sock is None:
-            self.connect()
-
-        # Simple netstrings implementation, which also assumes that there is
-        # only one response to our query
-        try:
-            self.sock.send(f"{len(cmd)}:{cmd},".encode('utf-8'))
-            while True:
-                try:
-                    colpos = self.buf.index(b':')
-                except ValueError as exc:
-                    # Not enough data, read some and try again
-                    recv = self.sock.recv(1024)
-                    if len(recv) == 0:
-                        raise ConnectionError("RTS hung up")
-                    self.buf += recv
-                    continue
-                length = int(self.buf[0:colpos])
-                start = colpos + 1
-                end = start + length
-                if len(self.buf) < end:
-                    # Not enough data, read some and try again
-                    recv = self.sock.recv(1024)
-                    if len(recv) == 0:
-                        raise ConnectionError("RTS hung up")
-                    self.buf += recv
-                    continue
-                res = self.buf[start:end]
-                self.buf = self.buf[end+1:] # +1 to skip the ,
-                return json.loads(res.decode("utf-8"))
-        except Exception as exc:
-            self.connect()
-            if retries > 0:
-                time.sleep(0.01)
-                return self.cmd(cmd, retries-1)
-            else:
-                raise ConnectionError("Unable to get data from acton rts")
+    buf = b""
+    # Simple netstrings implementation, which also assumes that there is
+    # only one response to our query
+    try:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(address)
+        sock.send(f"{len(cmd)}:{cmd},".encode('utf-8'))
+        while True:
+            try:
+                colpos = buf.index(b':')
+            except ValueError as exc:
+                # Not enough data, read some and try again
+                recv = sock.recv(1024)
+                if len(recv) == 0:
+                    raise ConnectionError("RTS hung up")
+                buf += recv
+                continue
+            length = int(buf[0:colpos])
+            start = colpos + 1
+            end = start + length
+            if len(buf) < end:
+                # Not enough data, read some and try again
+                recv = sock.recv(1024)
+                if len(recv) == 0:
+                    raise ConnectionError("RTS hung up")
+                buf += recv
+                continue
+            res = buf[start:end]
+            buf = buf[end+1:] # +1 to skip the ,
+            sock.close()
+            return json.loads(res.decode("utf-8"))
+    except Exception as exc:
+        sock.close()
+        if retries > 0:
+            time.sleep(0.01)
+            return mon_cmd(address, cmd, retries-1)
+        else:
+            raise ConnectionError("Unable to get data from acton rts")
 
 
 class Db:
@@ -98,6 +85,8 @@ class Db:
                 "-p", str(self.port), "-m", str(self.gossip_port),
                 "-s", f"127.0.0.1:{self.seed_port}"]
         self.p = subprocess.Popen(cmd, stdout=self.logfile, stderr=self.logfile)
+        time.sleep(0.1)
+        self.get_membership()
         for i in range(9999):
             if i > 100:
                 raise Exception("Unable to get membership")
@@ -111,6 +100,7 @@ class Db:
     def stop(self):
         self.p.kill()
         self.p.wait() # wait for the child process to exit
+        self.logfile.close()
 
 
     def get_vc(self):
@@ -118,13 +108,13 @@ class Db:
         return data["membership"]["view_id"]
 
     def get_membership(self):
-        ms = MonSock(self.mon_sock)
-        return ms.cmd("membership")
+        return mon_cmd(self.mon_sock, "membership")
 
 
 
 class DbCluster:
     def __init__(self, num=3, base_port=None):
+        self.log = logging.getLogger()
         self.num = num
         self.base_port = base_port
         if not self.base_port:
@@ -135,16 +125,15 @@ class DbCluster:
     def start(self):
         """Start up a cluster of num nodes and ensure that memberships look alright
         """
-        log = logging.getLogger()
 
-        log.debug("Starting database servers")
+        self.log.debug("Starting database servers")
         self.dbs=[Db(0, self.base_port)]
         self.dbs[0].start()
 
         allgood = True
 
         for i in range(1, self.num):
-            log.debug(f"Starting instance {i}")
+            self.log.debug(f"Starting instance {i}")
             # Create & start new instance
             dbn = Db(i, self.base_port)
             dbn.start()
@@ -154,22 +143,21 @@ class DbCluster:
             expected = None
             for db in self.dbs:
                 mbs = db.get_vc()
-                #log.debug(f"Membership info for {db}: {mbs}")
                 if expected is None:
-                    log.debug(f"Using Membership info from {db} as expected: {mbs}")
+                    self.log.debug(f"Using Membership info from {db} as expected: {mbs}")
                     expected = mbs
                 else:
                     if mbs == expected:
-                        log.debug(f"Membership info for {db} is as expected: {mbs}")
+                        self.log.debug(f"Membership info for {db} is as expected: {mbs}")
                         pass
                     else:
-                        log.error(f"Membership info for {db} seems incorrect: {mbs}")
+                        self.log.error(f"Membership info for {db} seems incorrect: {mbs}")
                         allgood = False
         return allgood
 
 
     def stop(self):
-        log.debug("Stopping database servers")
+        self.log.debug("Stopping database servers")
         for dbn in self.dbs:
             dbn.stop()
         return True
@@ -228,6 +216,7 @@ def test_app_recovery(base_port, replication_factor):
 
 
 def stderr_checker(line, p, s):
+    log = logging.getLogger()
     log.info(f"App stderr: {line}")
 
     if re.search("Assertion", line):
@@ -243,6 +232,7 @@ def stderr_checker(line, p, s):
 
 
 def run_cmd(cmd, cb_so=None, cb_se=None, cb_end=None, state=None):
+    log = logging.getLogger()
     log.debug(f"Starting application: {' '.join(cmd)}")
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     done = False
@@ -272,78 +262,123 @@ def run_cmd(cmd, cb_so=None, cb_se=None, cb_end=None, state=None):
     return p, state
 
 
-def test_app(base_port, replication_factor):
-    cmd = (["./test_db_app", "--rts-verbose", "--rts-ddb-replication", str(replication_factor)] +
-           get_db_args(base_port, replication_factor))
-
-    def so(line, p, s):
-        log.info(f"App output: {line}")
-        return False
 
 
-    state = {}
+class TestDbApps(unittest.TestCase):
+    replication_factor = 3
+    dbc = None
 
-    p, s = run_cmd(cmd, so, stderr_checker, state=state)
+    def setUp(self):
+        self.dbc = DbCluster(self.replication_factor)
+        self.dbc.start()
 
-    if p.returncode == 0:
-        log.debug("application exited successfully")
-        return True
-    else:
-        log.error(f"Non-0 return code: {p.returncode}")
-        return False
+    def tearDown(self):
+        self.dbc.stop()
 
+
+    def test_app(self):
+        cmd = ["./test_db_app", "--rts-verbose",
+               "--rts-ddb-replication", str(self.replication_factor)
+               ] + get_db_args(self.dbc.base_port, self.replication_factor)
+        p = subprocess.run(cmd, capture_output=True, timeout=3)
+
+        self.assertEqual(p.returncode, 0)
+
+
+    def test_app_recovery(self):
+        cmd = ["./test_db_recovery", "--rts-verbose",
+               "--rts-ddb-replication", str(self.replication_factor)
+               ] + get_db_args(self.dbc.base_port, self.replication_factor)
+        log = logging.getLogger()
+
+        def so1(line, p, s):
+            log.info(f"App output: {line}")
+            m = re.match("COUNT: (\d+)", line)
+            if m:
+                log.debug(f"Got count: {m.group(1)}")
+                if int(m.group(1)) != s["i"]:
+                    raise ValueError(f"Unexpected output from app, got {line} but expected {i}")
+                if s["i"] == 3:
+                    log.debug("Waiting somewhat")
+                    time.sleep(0.1)
+                    log.debug("Killing application")
+                    p.terminate()
+                s["i"] += 1
+
+            return False
+
+        def so2(line, p, s):
+            log.info(f"App output: {line}")
+            m = re.match("COUNT: (\d+)", line)
+            if m:
+                log.debug(f"Got count: {m.group(1)}")
+                if int(m.group(1)) == s["i"]:
+                    log.info(f"App resumed perfectly at {s['i']}")
+                elif int(m.group(1)) == s["i"]-1:
+                    log.info(f"Got higher than {s['i']-1}, deemed ok but seems we failed to snapshot last count?")
+                else:
+                    raise ValueError(f"Unexpected output from app, got {line} but expected {s['i']}")
+                s["i"] += 1
+            return False
+
+        state = {
+            "i": 1
+        }
+
+        p, s = run_cmd(cmd, so1, stderr_checker, state=state)
+        p, s = run_cmd(cmd, so2, stderr_checker, state=state)
+
+        self.assertEqual(p.returncode, 0)
+
+
+class TestDbAppsNoQuorum(unittest.TestCase):
+    def test_app(self):
+        cmd = ["./test_db_app", "--rts-verbose",
+               "--rts-ddb-replication", "3",
+               "--rts-ddb-host", "localhost",
+               "--rts-ddb-host", "localhost",
+               "--rts-ddb-host", "localhost"
+               ]
+        p = subprocess.run(cmd, capture_output=True, timeout=20)
+        self.assertTrue(re.search(r"No quorum", p.stderr.decode("utf-8")))
+        # TODO: really should not be 0, the program should not return at all! It
+        # should just halt until it reaches quorum again, which it will never
+        # do, thus should pause forever and we should get a timeout exception
+        self.assertEqual(p.returncode, 0)
+        # TODO: we should not see the normal program output since the program
+        # should not be able to make progress without DB quorum
+        #self.assertFalse(re.search(r"In final method", p.stdout.decode("utf-8")))
+
+    # This is what the above test case should look like when RTS is working
+    # properly!
+    @unittest.skip("RTS does not properly pause under lack of quorum")
+    def test_app_proper(self):
+        cmd = ["./test_db_app", "--rts-verbose",
+               "--rts-ddb-replication", "3",
+               "--rts-ddb-host", "localhost",
+               "--rts-ddb-host", "localhost",
+               "--rts-ddb-host", "localhost"
+               ]
+        with self.assertRaises(SomeException) as cm:
+            subprocess.run(cmd, capture_output=True, timeout=20)
+
+        exc = cm.exception
+        self.assertTrue(re.search(r"No quorum", exc.stderr.decode("utf-8")))
+        self.assertFalse(re.search(r"In final method", exc.stdout.decode("utf-8")))
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db-nodes", type=int, default=3)
-    parser.add_argument("--replication-factor", type=int)
     parser.add_argument("--repeat", type=int, default=1)
-    parser.add_argument("--test-app", action="store_true")
-    parser.add_argument("--test-recovery", action="store_true")
-    args = parser.parse_args()
+    parser.add_argument("--replication-factor", type=int, default=3)
+    args, unknown = parser.parse_known_args()
 
-    if args.replication_factor is None:
-        args.replication_factor = args.db_nodes
-
-    # set logging format
-    LOG_FORMAT = "%(asctime)s: %(module)-10s %(levelname)-8s %(message)s"
-    # setup basic logging
-    logging.basicConfig(format=LOG_FORMAT)
-
-    log = logging.getLogger()
-    log.setLevel(logging.INFO)
-    log.setLevel(logging.DEBUG)
+    TestDbApps.replication_factor = args.replication_factor
 
     allgood = True
-
     for i in range(args.repeat):
-        log.info(f"-- Round {i}")
-
-        dbc = DbCluster(args.db_nodes)
-
-        if not dbc.start():
-            allgood = False
-
-        if args.test_app:
-            try:
-                if not test_app(dbc.base_port, args.replication_factor):
-                    allgood = False
-            except Exception as exc:
-                log.error(exc)
-                allgood = False
-
-        if args.test_recovery:
-            try:
-                if not test_app_recovery(dbc.base_port, args.replication_factor):
-                    allgood = False
-            except Exception as exc:
-                log.exception(exc)
-                allgood = False
-
-        if not dbc.stop():
-            log.error("Something went wrong stopping DB Cluster")
+        if not unittest.main(argv=[sys.argv[0]] + unknown, exit=False):
             allgood = False
 
     if allgood:
