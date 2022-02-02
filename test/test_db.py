2cb9abee4a9f7b0e2a92951c2ae25f569f14fe7d
Kristian Larsson <kristian@spritelink.net>
2022-02-02 10:23:19 +0100

    Revamp DB interaction in tests
    With the new monitor interface on the DB we can use it to determine when
    the DB has started and ask about membership. Much better than tailing
    stdout and regex parsing its output.
    
    Further, we now compute a random base port which is used when starting
    the DB cluster and to feed the right arguments to the Acton RTS.

--- test/test_db.py
+++ test/test_db.py
@@ -1,61 +1,111 @@
 #!/usr/bin/env python3
 
+import json
+import locale
 import logging
 import os
-import locale
+import random
 import re
+import select
+import socket
 import subprocess
 import sys
 import time
-import select
 
 ACTONDB="../dist/bin/actondb"
 BASEPORT=32001
 
-def parse_membership(line):
-    m = re.match(".*Membership_agreement_msg.type=(?P<type>[^,]+), ack_status=(?P<ack_status>[^,]+), nonce=(?P<nonce>[0-9]+), Membership\((?P<membership>(Node\(.*?\))+), , VC\(.*?\)\), local_view_disagrees=(?P<local_view_disagrees>.)", line)
-    membership = []
-    if m is not None:
-        for node in re.findall(r"Node\((.*?)\)(, )?", m.group('membership')):
-            res = dict(map(lambda x: x.split('='), node[0].split(', ')))
-            membership.append(res)
-        return {
-            'type': m.group('type'),
-            'ack_status': m.group('ack_status'),
-            'nonce': m.group('nonce'),
-            'local_view_disagrees': m.group('local_view_disagrees'),
-            'membership': membership
-        }
-    raise ValueError("Unable to parse Membership agreement")
+
+def get_db_args(base_port, replication_factor):
+    return [item for sublist in map(lambda x: ("--rts-ddb-host", x), [f"127.0.0.1:{base_port+idx}" for idx in range(replication_factor)]) for item in sublist]
 
 
-def get_db_args(replication_factor):
-    return [item for sublist in map(lambda x: ("--rts-ddb-host", x), [f"127.0.0.1:{32000+idx}" for idx in range(replication_factor)]) for item in sublist]
+class MonSock:
+    """Communicate with the monitor interface of actondb or RTS using netstrings
+    encoded payloads
+    """
+    def __init__(self, address):
+        self.address = address
+        self.sock = None
+        self.buf = b""
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        try:
+            self.sock.connect(self.address)
+        except Exception as exc:
+            pass
+
+    def cmd(self, cmd, retries=5):
+        if self.sock is None:
+            self.connect()
+
+        # Simple netstrings implementation, which also assumes that there is
+        # only one response to our query
+        try:
+            self.sock.send(f"{len(cmd)}:{cmd},".encode('utf-8'))
+            while True:
+                try:
+                    colpos = self.buf.index(b':')
+                except ValueError as exc:
+                    # Not enough data, read some and try again
+                    recv = self.sock.recv(1024)
+                    if len(recv) == 0:
+                        raise ConnectionError("RTS hung up")
+                    self.buf += recv
+                    continue
+                length = int(self.buf[0:colpos])
+                start = colpos + 1
+                end = start + length
+                if len(self.buf) < end:
+                    # Not enough data, read some and try again
+                    recv = self.sock.recv(1024)
+                    if len(recv) == 0:
+                        raise ConnectionError("RTS hung up")
+                    self.buf += recv
+                    continue
+                res = self.buf[start:end]
+                self.buf = self.buf[end+1:] # +1 to skip the ,
+                return json.loads(res.decode("utf-8"))
+        except Exception as exc:
+            self.connect()
+            if retries > 0:
+                time.sleep(0.01)
+                return self.cmd(cmd, retries-1)
+            else:
+                raise ConnectionError("Unable to get data from acton rts")
+
 
 class Db:
+    """A single DB node
+    """
     def __repr__(self):
         return f"Db{self.idx}"
 
-    def __init__(self, idx):
+    def __init__(self, idx, base_port=32000):
         self.idx = idx
         self.name = f"Db{idx}"
-        #print(f"Starting {self.name}")
+        self.port = base_port + self.idx
+        self.seed_port = base_port + 100
+        self.gossip_port = self.seed_port + self.idx
+        self.mon_sock = f"db{self.idx}_mon"
+        self.logfile = open(f"db{self.idx}.log", "w")
         self.p = None
         self.running = False
 
     def start(self):
-        args = [ACTONDB, "-p", f"32{self.idx:03d}", "-m", f"34{self.idx:03d}",
-                "-s", "127.0.0.1:34000"]
-        self.p = subprocess.Popen(
-            [ACTONDB, "-p", f"32{self.idx:03d}", "-m", f"34{self.idx:03d}", "-s", "127.0.0.1:34000"],
-            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        os.set_blocking(self.p.stdout.fileno(), False)
-
-        while True:
-            line = self.p.stdout.readline().decode(locale.getpreferredencoding(False)).strip()
-            if re.match("SERVER: Started", line):
-                self.running = True
+        cmd = [ACTONDB, "--mon-socket-path", self.mon_sock,
+                "-p", str(self.port), "-m", str(self.gossip_port),
+                "-s", f"127.0.0.1:{self.seed_port}"]
+        self.p = subprocess.Popen(cmd, stdout=self.logfile, stderr=self.logfile)
+        for i in range(9999):
+            if i > 100:
+                raise Exception("Unable to get membership")
+            try:
+                self.get_membership()
                 break
+            except:
+                time.sleep(0.01)
 
 
     def stop(self):
@@ -63,21 +113,24 @@ class Db:
         self.p.wait() # wait for the child process to exit
 
 
-    def await_membership(self):
-        # Try reading & looking for our expected result for some time before
-        # giving up
-        st = time.time()
-        lines = []
-        # Read in all buffered lines
-        while True:
-            line = self.p.stdout.readline().decode(locale.getpreferredencoding(False)).strip()
-            if re.match("SERVER: Installed new agreed", line):
-                return parse_membership(line)
+    def get_vc(self):
+        data = self.get_membership()
+        return data["membership"]["view_id"]
+
+    def get_membership(self):
+        ms = MonSock(self.mon_sock)
+        return ms.cmd("membership")
+
 
 
 class DbCluster:
-    def __init__(self, num=3):
+    def __init__(self, num=3, base_port=None):
         self.num = num
+        self.base_port = base_port
+        if not self.base_port:
+            # compute random base port between 10000 to 60000 in increments of
+            # 200 ports, which allows us to run up to 100 DB nodes per test
+            self.base_port = random.randint(50, 300) * 200
 
     def start(self):
         """Start up a cluster of num nodes and ensure that memberships look alright
@@ -85,7 +138,7 @@ class DbCluster:
         log = logging.getLogger()
 
         log.debug("Starting database servers")
-        self.dbs=[Db(0)]
+        self.dbs=[Db(0, self.base_port)]
         self.dbs[0].start()
 
         allgood = True
@@ -93,24 +146,24 @@ class DbCluster:
         for i in range(1, self.num):
             log.debug(f"Starting instance {i}")
             # Create & start new instance
-            dbn = Db(i)
+            dbn = Db(i, self.base_port)
             dbn.start()
             self.dbs.append(dbn)
 
             # Now go through all existing nodes and ensure all have new membership
             expected = None
             for db in self.dbs:
-                mbs = db.await_membership()
+                mbs = db.get_vc()
                 #log.debug(f"Membership info for {db}: {mbs}")
                 if expected is None:
-                    log.debug(f"Using Membership info from {db} as expected")
+                    log.debug(f"Using Membership info from {db} as expected: {mbs}")
                     expected = mbs
                 else:
                     if mbs == expected:
-                        log.debug(f"Membership info for {db} is as expected")
+                        log.debug(f"Membership info for {db} is as expected: {mbs}")
                         pass
                     else:
-                        log.error(f"Membership info for {db} seems incorrect")
+                        log.error(f"Membership info for {db} seems incorrect: {mbs}")
                         allgood = False
         return allgood
 
@@ -122,9 +175,8 @@ class DbCluster:
         return True
 
 
-def test_app_recovery(replication_factor):
-    cmd = ["./test_db_recovery", "--rts-verbose", "--rts-ddb-replication", str(replication_factor)] + get_db_args(replication_factor)
-
+def test_app_recovery(base_port, replication_factor):
+    cmd = ["./test_db_recovery", "--rts-verbose", "--rts-ddb-replication", str(replication_factor)] + get_db_args(base_port, replication_factor)
 
 
     def so1(line, p, s):
@@ -220,9 +272,9 @@ def run_cmd(cmd, cb_so=None, cb_se=None, cb_end=None, state=None):
     return p, state
 
 
-def test_app(replication_factor):
+def test_app(base_port, replication_factor):
     cmd = (["./test_db_app", "--rts-verbose", "--rts-ddb-replication", str(replication_factor)] +
-           get_db_args(replication_factor))
+           get_db_args(base_port, replication_factor))
 
     def so(line, p, s):
         log.info(f"App output: {line}")
@@ -276,7 +328,7 @@ if __name__ == '__main__':
 
         if args.test_app:
             try:
-                if not test_app(args.replication_factor):
+                if not test_app(dbc.base_port, args.replication_factor):
                     allgood = False
             except Exception as exc:
                 log.error(exc)
@@ -284,7 +336,7 @@ if __name__ == '__main__':
 
         if args.test_recovery:
             try:
-                if not test_app_recovery(args.replication_factor):
+                if not test_app_recovery(dbc.base_port, args.replication_factor):
                     allgood = False
             except Exception as exc:
                 log.exception(exc)
