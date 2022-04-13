be9605b392484355302a888da4167a0f10b8ebae
Kristian Larsson <kristian@spritelink.net>
2022-04-13 12:13:21 +0200

    Align output columns
    It's much easier to read with aligned output columns and I think we all
    have wide screens so we'll align the file and line columns so the
    message is always in the same place.

--- deps/log.c
+++ deps/log.c
@@ -55,12 +55,12 @@ static void stdout_callback(log_Event *ev) {
   buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
 #ifdef LOG_USE_COLOR
   fprintf(
-    ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
+    ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%-20s:%5d:\x1b[0m ",
     buf, level_colors[ev->level], level_strings[ev->level],
     ev->file, ev->line);
 #else
   fprintf(
-    ev->udata, "%s %-5s %s:%d: ",
+    ev->udata, "%s %-5s %-20s:%5d: ",
     buf, level_strings[ev->level], ev->file, ev->line);
 #endif
   vfprintf(ev->udata, ev->fmt, ev->ap);
@@ -73,7 +73,7 @@ static void file_callback(log_Event *ev) {
   char buf[64];
   buf[strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", ev->time)] = '\0';
   fprintf(
-    ev->udata, "%s %-5s %s:%d: ",
+    ev->udata, "%s %-5s %-20s:%-5d: ",
     buf, level_strings[ev->level], ev->file, ev->line);
   vfprintf(ev->udata, ev->fmt, ev->ap);
   fprintf(ev->udata, "\n");
