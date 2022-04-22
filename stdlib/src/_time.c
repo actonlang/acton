bd3df537139ed2daf3de1fc680e7b75c8164b3ef
Kristian Larsson <kristian@spritelink.net>
2022-04-22 09:58:05 +0200

    Move C parts of time module to _time
    The time module, which is implemented in C, has been renamed to _time
    and a new time module with wrapper functions for all the existing
    functions in _time has been created. Thus the interface remains the same
    but the time module is now implemented in Acton, making it possible to
    add more things implemented in Acton! This is preparatory for planned
    improvements to the time module.

--- stdlib/src/_time.c
+++ stdlib/src/_time.c
@@ -1,6 +1,6 @@
-#include "time.h"
+#include "_time.h"
 
-$float time$$monotonic () {
+$float _time$$monotonic () {
     struct timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -8,7 +8,7 @@ $float time$$monotonic () {
     return to$float(ts.tv_sec + ts.tv_nsec);
 }
 
-$int time$$monotonic_ns () {
+$int _time$$monotonic_ns () {
     struct timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -16,7 +16,7 @@ $int time$$monotonic_ns () {
     return to$int(ts.tv_sec * 1000000000 + ts.tv_nsec);
 }
 
-$float time$$time () {
+$float _time$$time () {
     struct timespec ts;
     if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -24,7 +24,7 @@ $float time$$time () {
     return to$float(ts.tv_sec + 0.000000001*ts.tv_nsec);
 }
 
-$int time$$time_ns () {
+$int _time$$time_ns () {
     struct timespec ts;
     if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -33,8 +33,8 @@ $int time$$time_ns () {
 }
 
 
-int time$$done$ = 0;
-void time$$__init__ () {
-    if (time$$done$) return;
-    time$$done$ = 1;
+int _time$$done$ = 0;
+void _time$$__init__ () {
+    if (_time$$done$) return;
+    _time$$done$ = 1;
 }
