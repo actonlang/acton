e9544dc7f8bd94163dfa82fa125871df3469b7c2
Björn von Sydow <bjorn.von.sydow@gmail.com>
2022-05-19 20:17:46 +0200

    added type bytes

--- builtin/hash.h
+++ builtin/hash.h
@@ -4,6 +4,7 @@ long $float_hash($float v);
 long $complex_hash($complex c);
 
 long $string_hash($str s);
+long $bytes_hash($bytes s);
 
 long $pointer_hash($WORD w);
 long $tuple_hash($Hashable$tuple wit,$tuple tup);
