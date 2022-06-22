2dac08fc091905448f397c8f9a9c94f0b38d9749
Johan Nordlander <johan.nordlander@dataductus.se>
2022-06-22 16:34:03 +0200

    Removed more garbage.

--- test/regression_auto/import_actor__bf/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cc  -c -I/home/kll/terastream/acton/test/regression_build/import_actor/out -I/home/kll/terastream/acton/dist -o/home/kll/terastream/acton/test/regression_build/import_actor/out/rel/lib/'foo'.o /home/kll/terastream/acton/test/regression_build/import_actor/out/types/foo.c
-ar rcs /home/kll/terastream/acton/test/regression_build/import_actor/out/rel/lib/libActonProject.a /home/kll/terastream/acton/test/regression_build/import_actor/out/rel/lib/'foo'.o
