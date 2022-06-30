70af050ce073c515f44c027151793caa9a4dae9d
Kristian Larsson <kristian@spritelink.net>
2022-06-30 12:30:42 +0200

    Join time & _time module
    We used to have these split because at the time we figured that we would
    keep C code in the _time module and wrap it up in Acton code in time.
    Since then we have come up with the new concept of using NotImplemented
    and .ext.c so we can now rather easily combine C and Acton code in the
    same module!

--- stdlib/src/time.ext.c
+++ stdlib/src/time.ext.c
@@ -1,6 +1,5 @@
-#include "_time.h"
 
-$float _time$$monotonic () {
+$float time$$monotonic () {
     struct timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -8,7 +7,7 @@ $float _time$$monotonic () {
     return to$float(ts.tv_sec + ts.tv_nsec);
 }
 
-$int _time$$monotonic_ns () {
+$int time$$monotonic_ns () {
     struct timespec ts;
     if (clock_gettime(CLOCK_MONOTONIC, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -16,7 +15,7 @@ $int _time$$monotonic_ns () {
     return to$int(ts.tv_sec * 1000000000 + ts.tv_nsec);
 }
 
-$float _time$$time () {
+$float time$$time () {
     struct timespec ts;
     if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
@@ -24,17 +23,10 @@ $float _time$$time () {
     return to$float(ts.tv_sec + 0.000000001*ts.tv_nsec);
 }
 
-$int _time$$time_ns () {
+$int time$$time_ns () {
     struct timespec ts;
     if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
         $RAISE((($BaseException)$RuntimeError$new(to$str("Unable to get time"))));
     }
     return to$int(ts.tv_sec * 1000000000 + ts.tv_nsec);
-}
-
-
-int _time$$done$ = 0;
-void _time$$__init__ () {
-    if (_time$$done$) return;
-    _time$$done$ = 1;
 }
