06fdaf8b13bfca76ce23d292b13d1ca35b7e8a99
Johan Nordlander <johan.nordlander@dataductus.se>
2022-09-21 16:28:44 +0200

    Cleaned up defunct lambdas in env.

--- builtin/env.h
+++ builtin/env.h
@@ -36,19 +36,6 @@ typedef enum HandlerCase {nohandler, readhandler, connecthandler} HandlerCase;
 struct $l$1lambda;
 struct $l$2lambda;
 struct $l$3lambda;
-struct $l$4lambda;
-struct $l$5lambda;
-struct $l$6lambda;
-struct $l$7lambda;
-struct $l$8lambda;
-struct $l$9lambda;
-struct $l$10lambda;
-struct $l$11lambda;
-struct $l$12lambda;
-struct $l$13lambda;
-struct $l$14lambda;
-struct $l$15lambda;
-struct $l$16lambda;
 struct $WorldAuth;
 struct $Env;
 struct $Connection;
@@ -58,19 +45,6 @@ struct $ListenSocket;
 typedef struct $l$1lambda *$l$1lambda;
 typedef struct $l$2lambda *$l$2lambda;
 typedef struct $l$3lambda *$l$3lambda;
-typedef struct $l$4lambda *$l$4lambda;
-typedef struct $l$5lambda *$l$5lambda;
-typedef struct $l$6lambda *$l$6lambda;
-typedef struct $l$7lambda *$l$7lambda;
-typedef struct $l$8lambda *$l$8lambda;
-typedef struct $l$9lambda *$l$9lambda;
-typedef struct $l$10lambda *$l$10lambda;
-typedef struct $l$11lambda *$l$11lambda;
-typedef struct $l$12lambda *$l$12lambda;
-typedef struct $l$13lambda *$l$13lambda;
-typedef struct $l$14lambda *$l$14lambda;
-typedef struct $l$15lambda *$l$15lambda;
-typedef struct $l$16lambda *$l$16lambda;
 typedef struct $WorldAuth *$WorldAuth;
 typedef struct $Env *$Env;
 typedef struct $Connection *$Connection;
@@ -115,7 +89,7 @@ struct $l$3lambda$class {
     char *$GCINFO;
     int $class_id;
     $Super$class $superclass;
-    $NoneType (*__init__) ($l$3lambda, $Env, $str, $int, $function);
+    $NoneType (*__init__) ($l$3lambda, $Env, $int);
     void (*__serialize__) ($l$3lambda, $Serial$state);
     $l$3lambda (*__deserialize__) ($l$3lambda, $Serial$state);
     $bool (*__bool__) ($l$3lambda);
@@ -126,227 +100,7 @@ struct $l$3lambda$class {
 struct $l$3lambda {
     struct $l$3lambda$class *$class;
     $Env __self__;
-    $str host;
-    $int port;
-    $function cb;
-};
-struct $l$4lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$4lambda, $Env, $int, $function, $function);
-    void (*__serialize__) ($l$4lambda, $Serial$state);
-    $l$4lambda (*__deserialize__) ($l$4lambda, $Serial$state);
-    $bool (*__bool__) ($l$4lambda);
-    $str (*__str__) ($l$4lambda);
-    $str (*__repr__) ($l$4lambda);
-    $R (*__call__) ($l$4lambda, $Cont);
-};
-struct $l$4lambda {
-    struct $l$4lambda$class *$class;
-    $Env __self__;
-    $int port;
-    $function on_connect;
-    $function on_error;
-};
-struct $l$5lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$5lambda, $Env, $int);
-    void (*__serialize__) ($l$5lambda, $Serial$state);
-    $l$5lambda (*__deserialize__) ($l$5lambda, $Serial$state);
-    $bool (*__bool__) ($l$5lambda);
-    $str (*__str__) ($l$5lambda);
-    $str (*__repr__) ($l$5lambda);
-    $R (*__call__) ($l$5lambda, $Cont);
-};
-struct $l$5lambda {
-    struct $l$5lambda$class *$class;
-    $Env __self__;
     $int n;
-};
-struct $l$6lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$6lambda, $Env, $str);
-    void (*__serialize__) ($l$6lambda, $Serial$state);
-    $l$6lambda (*__deserialize__) ($l$6lambda, $Serial$state);
-    $bool (*__bool__) ($l$6lambda);
-    $str (*__str__) ($l$6lambda);
-    $str (*__repr__) ($l$6lambda);
-    $R (*__call__) ($l$6lambda, $Cont);
-};
-struct $l$6lambda {
-    struct $l$6lambda$class *$class;
-    $Env __self__;
-    $str nm;
-};
-struct $l$7lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$7lambda, $Env, $str);
-    void (*__serialize__) ($l$7lambda, $Serial$state);
-    $l$7lambda (*__deserialize__) ($l$7lambda, $Serial$state);
-    $bool (*__bool__) ($l$7lambda);
-    $str (*__str__) ($l$7lambda);
-    $str (*__repr__) ($l$7lambda);
-    $R (*__call__) ($l$7lambda, $Cont);
-};
-struct $l$7lambda {
-    struct $l$7lambda$class *$class;
-    $Env __self__;
-    $str nm;
-};
-struct $l$8lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$8lambda, $Connection, $bytes);
-    void (*__serialize__) ($l$8lambda, $Serial$state);
-    $l$8lambda (*__deserialize__) ($l$8lambda, $Serial$state);
-    $bool (*__bool__) ($l$8lambda);
-    $str (*__str__) ($l$8lambda);
-    $str (*__repr__) ($l$8lambda);
-    $R (*__call__) ($l$8lambda, $Cont);
-};
-struct $l$8lambda {
-    struct $l$8lambda$class *$class;
-    $Connection __self__;
-    $bytes s;
-};
-struct $l$9lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$9lambda, $Connection);
-    void (*__serialize__) ($l$9lambda, $Serial$state);
-    $l$9lambda (*__deserialize__) ($l$9lambda, $Serial$state);
-    $bool (*__bool__) ($l$9lambda);
-    $str (*__str__) ($l$9lambda);
-    $str (*__repr__) ($l$9lambda);
-    $R (*__call__) ($l$9lambda, $Cont);
-};
-struct $l$9lambda {
-    struct $l$9lambda$class *$class;
-    $Connection __self__;
-};
-struct $l$10lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$10lambda, $Connection, $function, $function);
-    void (*__serialize__) ($l$10lambda, $Serial$state);
-    $l$10lambda (*__deserialize__) ($l$10lambda, $Serial$state);
-    $bool (*__bool__) ($l$10lambda);
-    $str (*__str__) ($l$10lambda);
-    $str (*__repr__) ($l$10lambda);
-    $R (*__call__) ($l$10lambda, $Cont);
-};
-struct $l$10lambda {
-    struct $l$10lambda$class *$class;
-    $Connection __self__;
-    $function cb1;
-    $function cb2;
-};
-struct $l$11lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$11lambda, $RFile);
-    void (*__serialize__) ($l$11lambda, $Serial$state);
-    $l$11lambda (*__deserialize__) ($l$11lambda, $Serial$state);
-    $bool (*__bool__) ($l$11lambda);
-    $str (*__str__) ($l$11lambda);
-    $str (*__repr__) ($l$11lambda);
-    $R (*__call__) ($l$11lambda, $Cont);
-};
-struct $l$11lambda {
-    struct $l$11lambda$class *$class;
-    $RFile __self__;
-};
-struct $l$12lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$12lambda, $RFile);
-    void (*__serialize__) ($l$12lambda, $Serial$state);
-    $l$12lambda (*__deserialize__) ($l$12lambda, $Serial$state);
-    $bool (*__bool__) ($l$12lambda);
-    $str (*__str__) ($l$12lambda);
-    $str (*__repr__) ($l$12lambda);
-    $R (*__call__) ($l$12lambda, $Cont);
-};
-struct $l$12lambda {
-    struct $l$12lambda$class *$class;
-    $RFile __self__;
-};
-struct $l$13lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$13lambda, $WFile, $str);
-    void (*__serialize__) ($l$13lambda, $Serial$state);
-    $l$13lambda (*__deserialize__) ($l$13lambda, $Serial$state);
-    $bool (*__bool__) ($l$13lambda);
-    $str (*__str__) ($l$13lambda);
-    $str (*__repr__) ($l$13lambda);
-    $R (*__call__) ($l$13lambda, $Cont);
-};
-struct $l$13lambda {
-    struct $l$13lambda$class *$class;
-    $WFile __self__;
-    $str s;
-};
-struct $l$14lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$14lambda, $WFile);
-    void (*__serialize__) ($l$14lambda, $Serial$state);
-    $l$14lambda (*__deserialize__) ($l$14lambda, $Serial$state);
-    $bool (*__bool__) ($l$14lambda);
-    $str (*__str__) ($l$14lambda);
-    $str (*__repr__) ($l$14lambda);
-    $R (*__call__) ($l$14lambda, $Cont);
-};
-struct $l$14lambda {
-    struct $l$14lambda$class *$class;
-    $WFile __self__;
-};
-struct $l$15lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$15lambda, $function);
-    void (*__serialize__) ($l$15lambda, $Serial$state);
-    $l$15lambda (*__deserialize__) ($l$15lambda, $Serial$state);
-    $bool (*__bool__) ($l$15lambda);
-    $str (*__str__) ($l$15lambda);
-    $str (*__repr__) ($l$15lambda);
-    $R (*__call__) ($l$15lambda, $Cont);
-};
-struct $l$15lambda {
-    struct $l$15lambda$class *$class;
-    $function on_error;
-};
-struct $l$16lambda$class {
-    char *$GCINFO;
-    int $class_id;
-    $Super$class $superclass;
-    $NoneType (*__init__) ($l$16lambda, $ListenSocket);
-    void (*__serialize__) ($l$16lambda, $Serial$state);
-    $l$16lambda (*__deserialize__) ($l$16lambda, $Serial$state);
-    $bool (*__bool__) ($l$16lambda);
-    $str (*__str__) ($l$16lambda);
-    $str (*__repr__) ($l$16lambda);
-    $R (*__call__) ($l$16lambda, $Cont);
-};
-struct $l$16lambda {
-    struct $l$16lambda$class *$class;
-    $ListenSocket __self__;
 };
 struct $WorldAuth$class {
     char *$GCINFO;
@@ -368,33 +122,7 @@ $l$1lambda $l$1lambda$new($Env, $str);
 extern struct $l$2lambda$class $l$2lambda$methods;
 $l$2lambda $l$2lambda$new($Env, $function);
 extern struct $l$3lambda$class $l$3lambda$methods;
-$l$3lambda $l$3lambda$new($Env, $str, $int, $function);
-extern struct $l$4lambda$class $l$4lambda$methods;
-$l$4lambda $l$4lambda$new($Env, $int, $function, $function);
-extern struct $l$5lambda$class $l$5lambda$methods;
-$l$5lambda $l$5lambda$new($Env, $int);
-extern struct $l$6lambda$class $l$6lambda$methods;
-$l$6lambda $l$6lambda$new($Env, $str);
-extern struct $l$7lambda$class $l$7lambda$methods;
-$l$7lambda $l$7lambda$new($Env, $str);
-extern struct $l$8lambda$class $l$8lambda$methods;
-$l$8lambda $l$8lambda$new($Connection, $bytes);
-extern struct $l$9lambda$class $l$9lambda$methods;
-$l$9lambda $l$9lambda$new($Connection);
-extern struct $l$10lambda$class $l$10lambda$methods;
-$l$10lambda $l$10lambda$new($Connection, $function, $function);
-extern struct $l$11lambda$class $l$11lambda$methods;
-$l$11lambda $l$11lambda$new($RFile);
-extern struct $l$12lambda$class $l$12lambda$methods;
-$l$12lambda $l$12lambda$new($RFile);
-extern struct $l$13lambda$class $l$13lambda$methods;
-$l$13lambda $l$13lambda$new($WFile, $str);
-extern struct $l$14lambda$class $l$14lambda$methods;
-$l$14lambda $l$14lambda$new($WFile);
-extern struct $l$15lambda$class $l$15lambda$methods;
-$l$15lambda $l$15lambda$new($function);
-extern struct $l$16lambda$class $l$16lambda$methods;
-$l$16lambda $l$16lambda$new($ListenSocket);
+$l$3lambda $l$3lambda$new($Env, $int);
 extern struct $WorldAuth$class $WorldAuth$methods;
 // END GENERATED __builtin__.act
 ///////////////////////////////////////////////////////////////////////////////////////////
