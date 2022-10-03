aee58832419afb8b95795fc36e957e5e04f8e4a3
Kristian Larsson <kristian@spritelink.net>
2022-10-03 12:53:26 +0200

    Revamp remaining RTS DB tests
    Rewrite the two remaining RTS DB test; the simple test app and the TCP
    client resume test in the style of the TCP server test, which I
    previously rewrote to use a new test orchestrator (written in Acton).
    
    Pretty messy code, though it works and the tests execute very fast and
    print most relevant debug information if anything goes wrong.
    
    I'm able to run many test iterations without problems!

---
+++ test/rts_db/test_db_app.ext.c
@@ -0,0 +1,7 @@
+void test_db_app$$__ext_init__() {
+    // NOP
+}
+
+$int test_db_app$$Tester$p_id (test_db_app$$Tester __self__, process$$Process p) {
+  return to$int(p->$globkey);
+}
---
+++ test/rts_db/test_tcp_client.ext.c
@@ -0,0 +1,7 @@
+void test_tcp_client$$__ext_init__() {
+    // NOP
+}
+
+$int test_tcp_client$$Tester$p_id (test_tcp_client$$Tester __self__, process$$Process p) {
+  return to$int(p->$globkey);
+}
