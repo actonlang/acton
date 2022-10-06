c438d4803fc14ba1ee50f49224bad197957a712b
Kristian Larsson <kristian@spritelink.net>
2022-10-06 14:59:20 +0200

    Add acton.rts.rss
    Returns RSS!

---
+++ stdlib/src/acton/rts.ext.c
@@ -0,0 +1,17 @@
+#include <uv.h>
+
+void acton$rts$$__ext_init__() {
+    // NOP
+}
+
+$NoneType acton$rts$$sleep ($float sleep_time) {
+    int to_sleep = sleep_time->val*1000000;
+    usleep(to_sleep);
+    return $None;
+}
+
+$int acton$rts$$rss ($WorldAuth auth) {
+    size_t rsm;
+    int r = uv_resident_set_memory(&rsm);
+    return to$int(rsm);
+}
