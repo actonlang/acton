bad410ceeabbabc314a43c477c4b0c7f1f927249
Björn von Sydow <bjorn.von.sydow@gmail.com>
2023-05-30 09:51:03 +0200

    changed module math to regular stdlib form

---
+++ stdlib/src/math.ext.c
@@ -0,0 +1,49 @@
+B_float mathQ_RealFunsD_floatD_sqrt(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(sqrt(x->val));
+}
+B_float mathQ_RealFunsD_floatD_exp(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(exp(x->val));
+}
+B_float mathQ_RealFunsD_floatD_log(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(log(x->val));
+}
+B_float mathQ_RealFunsD_floatD_sin(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(sin(x->val));
+}
+B_float mathQ_RealFunsD_floatD_cos(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(cos(x->val));
+}
+B_float mathQ_RealFunsD_floatD_tan(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(tan(x->val));
+}
+B_float mathQ_RealFunsD_floatD_asin(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(asin(x->val));
+}
+B_float mathQ_RealFunsD_floatD_acos(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(acos(x->val));
+}
+B_float mathQ_RealFunsD_floatD_atan(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(atan(x->val));
+}
+B_float mathQ_RealFunsD_floatD_sinh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(sinh(x->val));
+}
+B_float mathQ_RealFunsD_floatD_cosh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(cosh(x->val));
+}
+B_float mathQ_RealFunsD_floatD_tanh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(tanh(x->val));
+}
+B_float mathQ_RealFunsD_floatD_asinh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(asinh(x->val));
+}
+B_float mathQ_RealFunsD_floatD_acosh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(acosh(x->val));
+}
+B_float mathQ_RealFunsD_floatD_atanh(mathQ_RealFunsD_float wit, B_float x) {
+  return to$float(atanh(x->val));
+}
+
+void mathQ___ext_init__() {
+    //NOP
+};
