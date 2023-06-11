49b2918924b8d8d5b277f891b7991004ee4501d6
Kristian Larsson <kristian@spritelink.net>
2023-06-11 14:12:43 +0200

    Add libuuid extracted from util-linux
    Rather than pull down the entirety of util-linux, which is a very large
    repo, we include the source files of just libuuid, although we have to
    include some dependencies it has on other parts of util-linux, like
    md5/sha1.

---
+++ deps/libuuid/build.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const print = @import("std").debug.print;
+
+pub fn build(b: *std.build.Builder) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "uuid",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const source_files = [_][]const u8{
+        "src/clear.c",
+        "src/compare.c",
+        "src/copy.c",
+        "src/gen_uuid.c",
+        "src/isnull.c",
+        "src/pack.c",
+        "src/parse.c",
+        "src/predefined.c",
+        "src/unpack.c",
+        "src/unparse.c",
+        "src/uuid_time.c",
+        // common
+        "src/md5.c",
+        "src/sha1.c",
+        "src/randutils.c",
+    };
+
+    lib.addCSourceFiles(&source_files, &[_][]const u8{
+        "-DHAVE_NANOSLEEP",
+        "-DHAVE_SYS_FILE_H",
+        "-DUSLEEP",
+    });
+    lib.addIncludePath("include");
+    lib.linkLibC();
+    b.installFile("src/uuid.h", "include/uuid/uuid.h");
+    b.installArtifact(lib);
+}
