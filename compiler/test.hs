c6435cec126a3a8901122bb5e09232701fec1c67
Kristian Larsson <kristian@spritelink.net>
2023-06-26 10:58:39 +0200

    Set cache directory for test builds
    This doesn't do much for normal builds but with cross-compilation or
    similar we should save significant amount of time.

--- compiler/test.hs
+++ compiler/test.hs
@@ -289,7 +289,7 @@ buildThing opts thing = do
     projPath <- canonicalizePath thing
     curDir <- getCurrentDirectory
     let wd = if proj then projPath else curDir
-    let actCmd    = (id actonc) ++ " " ++ (if proj then "build " else thing) ++ " --always-build " ++ opts
+    let actCmd    = (id actonc) ++ " " ++ (if proj then "build " else thing) ++ " --always-build --cache ~/.cache/acton/test-build-cache " ++ opts
     (returnCode, cmdOut, cmdErr) <- readCreateProcessWithExitCode (shell $ actCmd){ cwd = Just wd } ""
     return (returnCode, cmdOut, cmdErr)
 
