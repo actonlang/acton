eee28cd2932d4793783e029f06493c6904b79b2c
Johan Nordlander <johan.nordlander@dataductus.se>
2024-02-28 12:51:59 +0100

    Switched to platform-independent fixed int size representations.

--- base/builtin/u32.h
+++ base/builtin/u32.h
@@ -1,10 +1,10 @@
 struct B_u32 {
     struct B_u32G_class *$class;
-    unsigned int val;
+    uint32_t val;
 };
 
-B_u32 toB_u32(unsigned int n);
-unsigned int fromB_u32(B_u32 n);
+B_u32 toB_u32(uint32_t n);
+uint32_t fromB_u32(B_u32 n);
 
 B_u32 B_u32G_new(B_atom a, B_int base);
 
@@ -12,4 +12,4 @@ B_u32 B_u32G_new(B_atom a, B_int base);
 #define u32_FLOORDIV(a,b)  ( {if (b==0) $RAISE((B_BaseException)$NEW(B_ZeroDivisionError,to$str("u32 floordiv: division by zero")));  a/b;} )
 #define u32_MOD(a,b)       ( {if (b==0) $RAISE((B_BaseException)$NEW(B_ZeroDivisionError,to$str("u32 mod: division by zero"))); a%b;} )
 
-unsigned int u32_pow(unsigned int a, unsigned int b);
+uint32_t u32_pow(uint32_t a, uint32_t b);
