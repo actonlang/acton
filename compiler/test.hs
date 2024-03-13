ce467e21bf5d59fe5894c3cf3106ee07eeb1a71e
Kristian Larsson <kristian@spritelink.net>
2024-03-13 21:47:49 +0100

    Run tests in --dev mode
    Compiles are faster (quicker dev cycle!) and we get asserts.

--- compiler/test.hs
+++ compiler/test.hs
@@ -314,7 +314,7 @@ buildThing opts thing = do
     projPath <- canonicalizePath thing
     curDir <- getCurrentDirectory
     let wd = if proj then projPath else curDir
-    let actCmd    = (id actonc) ++ " " ++ (if proj then "build " else thing) ++ " --always-build " ++ opts
+    let actCmd    = (id actonc) ++ " " ++ (if proj then "build " else thing) ++ " --dev --always-build " ++ opts
     (returnCode, cmdOut, cmdErr) <- readCreateProcessWithExitCode (shell $ actCmd){ cwd = Just wd } ""
     return (returnCode, cmdOut, cmdErr)
 
