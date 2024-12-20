8f2e36afea7ff5bed782cf0d49132530245e1cd4
Kristian Larsson <k@centor.se>
2024-12-20 12:30:17 +0100

    Code formatting fixes

--- base/src/process.ext.c
+++ base/src/process.ext.c
@@ -31,7 +31,7 @@ void exit_handler(uv_process_t *req, int64_t exit_status, int term_signal) {
 }
 
 void read_stderr(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
-    if (nread < 0){
+    if (nread < 0) {
         if (nread == UV_EOF) {
             uv_close((uv_handle_t *)stream, NULL);
         }
@@ -46,7 +46,7 @@ void read_stderr(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
 }
 
 void read_stdout(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
-    if (nread < 0){
+    if (nread < 0) {
         if (nread == UV_EOF) {
             uv_close((uv_handle_t *)stream, NULL);
         }
@@ -60,11 +60,11 @@ void read_stdout(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
     }
 }
 
-$R processQ_ProcessD_aidG_local (processQ_Process self, $Cont c$cont) {
+$R processQ_ProcessD_aidG_local(processQ_Process self, $Cont c$cont) {
     return $R_CONT(c$cont, to$int(self->$globkey));
 }
 
-$R processQ_ProcessD__create_processG_local (processQ_Process self, $Cont c$cont) {
+$R processQ_ProcessD__create_processG_local(processQ_Process self, $Cont c$cont) {
     pin_actor_affinity();
     struct process_data *process_data = acton_calloc(1, sizeof(struct process_data));
     process_data->process = self;
@@ -79,7 +79,7 @@ $R processQ_ProcessD__create_processG_local (processQ_Process self, $Cont c$cont
 
     req->data = process_data;
 
-    char **args = (char **)acton_malloc((self->cmd->length+1) * sizeof(char *));
+    char **args = (char **)acton_malloc((self->cmd->length + 1) * sizeof(char *));
 
     int i;
     for (i = 0; i < self->cmd->length; i++) {
@@ -89,12 +89,12 @@ $R processQ_ProcessD__create_processG_local (processQ_Process self, $Cont c$cont
 
     if (self->workdir != B_None) {
         options->cwd = fromB_str(self->workdir);
-    };
+    }
 
     if (self->env == B_None) {
         options->env = NULL;
     } else {
-        char **env = (char **)acton_calloc((self->env->numelements+1), sizeof(char *));
+        char **env = (char **)acton_calloc((self->env->numelements + 1), sizeof(char *));
         B_IteratorD_dict_items iter = $NEW(B_IteratorD_dict_items, self->env);
         B_tuple item;
         for (i=0; i < self->env->numelements; i++) {
@@ -121,13 +121,13 @@ $R processQ_ProcessD__create_processG_local (processQ_Process self, $Cont c$cont
     options->stdio = process_stdio;
 
     process_stdio[0].flags = UV_CREATE_PIPE | UV_READABLE_PIPE;
-    process_stdio[0].data.stream = (uv_stream_t*)&(process_data->stdin_pipe);
+    process_stdio[0].data.stream = (uv_stream_t *)&(process_data->stdin_pipe);
 
     process_stdio[1].flags = UV_CREATE_PIPE | UV_WRITABLE_PIPE;
-    process_stdio[1].data.stream = (uv_stream_t*)&(process_data->stdout_pipe);
+    process_stdio[1].data.stream = (uv_stream_t *)&(process_data->stdout_pipe);
 
     process_stdio[2].flags = UV_CREATE_PIPE | UV_WRITABLE_PIPE;
-    process_stdio[2].data.stream = (uv_stream_t*)&(process_data->stderr_pipe);
+    process_stdio[2].data.stream = (uv_stream_t *)&(process_data->stderr_pipe);
 
     options->stdio_count = 3;
 
@@ -135,18 +135,19 @@ $R processQ_ProcessD__create_processG_local (processQ_Process self, $Cont c$cont
     options->file = args[0];
     options->args = args;
 
-    int r;
-    if ((r = uv_spawn(get_uv_loop(), req, options))) {
+    int r = uv_spawn(get_uv_loop(), req, options);
+    if (r != 0) {
         char errmsg[1024] = "Failed to spawn process: ";
-        uv_strerror_r(r, errmsg + strlen(errmsg), sizeof(errmsg)-strlen(errmsg));
+        uv_strerror_r(r, errmsg + strlen(errmsg), sizeof(errmsg) - strlen(errmsg));
         log_warn(errmsg);
         $action2 f = ($action2)self->on_error;
         f->$class->__asyn__(f, process_data->process, to$str(errmsg));
     }
     // TODO: do we need to do some magic to read any data produced before this
     // callback is installed?
-    uv_read_start((uv_stream_t*)&process_data->stdout_pipe, alloc_buffer, read_stdout);
-    uv_read_start((uv_stream_t*)&process_data->stderr_pipe, alloc_buffer, read_stderr);
+
+    uv_read_start((uv_stream_t *)&process_data->stdout_pipe, alloc_buffer, read_stdout);
+    uv_read_start((uv_stream_t *)&process_data->stderr_pipe, alloc_buffer, read_stderr);
 
     return $R_CONT(c$cont, B_None);
 }
@@ -155,7 +156,7 @@ void close_cb(uv_handle_t *handle) {
     // TODO: clean something up?
 }
 
-$R processQ_ProcessD_done_writingG_local (processQ_Process self, $Cont c$cont) {
+$R processQ_ProcessD_done_writingG_local(processQ_Process self, $Cont c$cont) {
     uv_process_t *p = (uv_process_t *)from$int(self->_p);
     struct process_data *process_data = (struct process_data *)p->data;
     uv_handle_t *stdin_handle = (uv_handle_t *)&process_data->stdin_pipe;
@@ -163,18 +164,18 @@ $R processQ_ProcessD_done_writingG_local (processQ_Process self, $Cont c$cont) {
     return $R_CONT(c$cont, B_None);
 }
 
-$R processQ_ProcessD_pidG_local (processQ_Process self, $Cont c$cont) {
+$R processQ_ProcessD_pidG_local(processQ_Process self, $Cont c$cont) {
     uv_process_t *p = (uv_process_t *)from$int(self->_p);
     return $R_CONT(c$cont, (B_atom)to$int(p->pid));
 }
 
-$R processQ_ProcessD_signalG_local (processQ_Process self, $Cont c$cont, B_int signal) {
+$R processQ_ProcessD_signalG_local(processQ_Process self, $Cont c$cont, B_int signal) {
     uv_process_t *p = (uv_process_t *)from$int(self->_p);
     uv_process_kill(p, from$int(signal));
     return $R_CONT(c$cont, B_None);
 }
 
-$R processQ_ProcessD_writeG_local (processQ_Process self, $Cont c$cont, B_bytes data) {
+$R processQ_ProcessD_writeG_local(processQ_Process self, $Cont c$cont, B_bytes data) {
     uv_process_t *p = (uv_process_t *)from$int(self->_p);
 
     uv_write_t *req = (uv_write_t *)acton_malloc(sizeof(uv_write_t));
@@ -186,7 +187,7 @@ $R processQ_ProcessD_writeG_local (processQ_Process self, $Cont c$cont, B_bytes 
     int r = uv_write(req, stdin_handle, &buf, 1, NULL);
     if (r != 0) {
         char errmsg[1024] = "Error writing to stdin of process: ";
-        uv_strerror_r(r, errmsg + strlen(errmsg), sizeof(errmsg)-strlen(errmsg));
+        uv_strerror_r(r, errmsg + strlen(errmsg), sizeof(errmsg) - strlen(errmsg));
         log_warn(errmsg);
     }
 
