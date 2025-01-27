73ef8c98cba7f96b7fda0e602193ab8981091d89
Kristian Larsson <k@centor.se>
2025-01-27 08:10:34 +0100

    Remove generated headers & binaries
    These were checked in by mistake.

--- compiler/actonc/test/project/simple/out/include/out/types/a.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "builtin/builtin.h"
-#include "rts/rts.h"
-#include "out/types/b.h"
-void aQ___init__ ();
--- compiler/actonc/test/project/simple/out/include/out/types/b.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "builtin/builtin.h"
-#include "rts/rts.h"
-B_NoneType bQ_foo ();
-void bQ___init__ ();
