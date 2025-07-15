d09f106aed16790156fcd3383f2e215d3efa6fd1
Kristian Larsson <k@centor.se>
2025-07-15 15:03:50 +0700

    Avoid superfluous malloc in skip_chars

--- base/builtin/str.c
+++ base/builtin/str.c
@@ -229,7 +229,7 @@ static int char_no(B_str text,int i) {
     return res;
 }
 
-static unsigned char *skip_chars(unsigned char* start,int n, int isascii) {
+static unsigned char *skip_chars(unsigned char* start, int n, int isascii) {
     unsigned char *res = start;
     if (isascii)
         return start+n;
@@ -275,7 +275,6 @@ static int get_index(int i, int nchars) {
 
 static int fix_start_end(int nchars, B_int *start, B_int *end) {
     if (*start==NULL) {
-        *start = acton_malloc(sizeof(struct B_int));
         *start = to$int(0);
     } else {
         int st = from$int(*start);
@@ -287,7 +286,6 @@ static int fix_start_end(int nchars, B_int *start, B_int *end) {
         *start = to$int(st);
     }
     if (*end==NULL) {
-        *end = acton_malloc(sizeof(struct B_int));
         *end = to$int(nchars);
     } else {
         int en = from$int(*end);
