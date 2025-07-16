593f140d07ca8859fb3d2304d5cc81355319a77f
Björn von Sydow <bjorn.von.sydow@gmail.com>
2025-07-16 15:44:07 +0200

    changed range to be an iterator

--- compiler/lib/src/Acton/Prim.hs
+++ compiler/lib/src/Acton/Prim.hs
@@ -146,6 +146,7 @@ primMkDict          = gPrim "mkDict"
 primAnnot           = gPrim "annot"
 
 primUGetItem        = gPrim "listD_U__getitem__"
+primUNext           = gPrim "rangeD_U__next__"
 
 annot t_ann ann t e = eCall (tApp (eQVar primAnnot) [t_ann, t]) [ann, e]
 
@@ -232,7 +233,8 @@ primEnv             = [     (noq primASYNCf,        NDef scASYNCf NoDec Nothing)
                             (noq primMkSet,         NDef scMkSet NoDec Nothing),
                             (noq primMkDict,        NDef scMkDict NoDec Nothing),
                             (noq primAnnot,         NDef scAnnot NoDec Nothing),
-                            (noq primUGetItem,      NDef scUGetItem NoDec Nothing)
+                            (noq primUGetItem,      NDef scUGetItem NoDec Nothing),
+                            (noq primUNext,         NDef scUNext NoDec Nothing)
                       ]
 
 --  class $Cont[T] (value): pass
@@ -555,6 +557,11 @@ scAnnot             = tSchema [quant a, quant b] tAnnot
 scUGetItem          = tSchema [quant a] tUGetItem
   where tUGetItem   = tFun fxPure (posRow (tList (tVar a)) (posRow tI64 posNil)) kwdNil (tVar a)
         a           = TV KType $ name "A"
+
+-- $rangeD_U__next__ : [A] => (Iterator[A]) -> A   will only be used to replace B_IteratorD_range.__next__
+scUNext             = tSchema [quant a] tUNext
+  where tUNext      = tFun fxMut (posRow (tIterator (tVar a)) posNil) kwdNil tI64
+        a           = TV KType $ name "A" 
         
 --  $WRAP           : [A,B,C] => ($Actor, proc(*A,**B)->C) -> action(*A,**B)->C
 scWRAP              = tSchema [quant a, quant b, quant c] tWRAP
