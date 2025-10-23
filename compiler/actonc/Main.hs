8fa6533357111a3d4a7f902fe062cd74517ea807
Marko Zagožen <marko.zagozen@gmail.com>
2025-10-23 14:17:51 +0200

    Make Acton CLI responsible for build.zig, build.zig.zon
    Acton CLI now replaces the SYSPATH placeholder in build.zig.zon (from
    dist/builder/build.zig.zon) with the correct relative path to the Acton
    distribution. Previously this was only done in actonc, which made way
    for a race condition when building projects with a diamond-shaped
    dependency tree.
    
    actonc now *does not* touch build.zig and build.zig.zon when started as
    a sub-compiler from Acton CLI (--sub flag). When started as a standalone
    compiler, for example when building Acton CLI itself, it still ensures
    that both files exist.

--- compiler/actonc/Main.hs
+++ compiler/actonc/Main.hs
@@ -1201,7 +1201,6 @@ zigBuild env gopts opts paths tasks binTasks = do
     iff (not (quiet gopts opts)) $ putStrLn("  Final compilation step")
     timeStart <- getTime Monotonic
 
-    -- custom build.zig ?
     homeDir <- getHomeDirectory
     let local_cache_dir = joinPath [ homeDir, ".cache", "acton", "zig-local-cache" ]
         global_cache_dir = joinPath [ homeDir, ".cache", "acton", "zig-global-cache" ]
@@ -1217,28 +1216,35 @@ zigBuild env gopts opts paths tasks binTasks = do
                            ("aarch64":"linux":_)   -> " -Dcpu=cortex_a72 "
                            ("x86_64":_:_)          -> " -Dcpu=westmere "
                            (_:_:_)                 -> defCpu
-        buildZigPath = joinPath [projPath paths, "build.zig"]
-        buildZonPath = joinPath [projPath paths, "build.zig.zon"]
 
-    buildZigExists <- doesFileExist buildZigPath
-    buildZonExists <- doesFileExist buildZonPath
-    -- Compute relative path from current directory (projPath paths)
-    let relativeSysPath = makeAlwaysRelative (projPath paths) (sysPath paths)
-    iff (not buildZigExists) $ do
-      let distBuildZigPath = joinPath [(sysPath paths), "builder", "build.zig"]
-      copyFile distBuildZigPath buildZigPath
-    if buildZonExists
+    -- If actonc runs as a standalone compiler (not a sub-compiler from Acton CLI),
+    -- then we may need to generate build.zig and build.zig.zon. We do not do so
+    -- unconditionally, however, to avoid overwriting any customizations.
+    if not (C.sub gopts)
       then do
-        curBuildZon <- readFile buildZonPath
-        let newBuildZon = replace "SYSPATH" relativeSysPath curBuildZon
-        removeFile buildZonPath
-          `catch` handleNotExists
-        writeFile buildZonPath newBuildZon
-      else do
-        let distBuildZonPath = joinPath [(sysPath paths), "builder", "build.zig.zon"]
-        distBuildZon <- readFile distBuildZonPath
-        let buildZon = replace "SYSPATH" relativeSysPath distBuildZon
-        writeFile buildZonPath buildZon
+        let buildZigPath = joinPath [projPath paths, "build.zig"]
+            buildZonPath = joinPath [projPath paths, "build.zig.zon"]
+
+        buildZigExists <- doesFileExist buildZigPath
+        buildZonExists <- doesFileExist buildZonPath
+        -- Compute relative path from current directory (projPath paths)
+        let relativeSysPath = makeAlwaysRelative (projPath paths) (sysPath paths)
+        iff (not buildZigExists) $ do
+          let distBuildZigPath = joinPath [(sysPath paths), "builder", "build.zig"]
+          copyFile distBuildZigPath buildZigPath
+        if buildZonExists
+          then do
+            curBuildZon <- readFile buildZonPath
+            let newBuildZon = replace "SYSPATH" relativeSysPath curBuildZon
+            removeFile buildZonPath
+              `catch` handleNotExists
+            writeFile buildZonPath newBuildZon
+          else do
+            let distBuildZonPath = joinPath [(sysPath paths), "builder", "build.zig.zon"]
+            distBuildZon <- readFile distBuildZonPath
+            let buildZon = replace "SYSPATH" relativeSysPath distBuildZon
+            writeFile buildZonPath buildZon
+      else return ()
 
     let zigCmdBase = zig paths ++ " build " ++ " --cache-dir " ++ local_cache_dir ++
                  " --global-cache-dir " ++ global_cache_dir ++
