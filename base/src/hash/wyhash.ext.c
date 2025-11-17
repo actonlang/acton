ca849d3f5f1d320cd6393ac90d4389763c85d04d
Björn von Sydow <bjorn.von.sydow@gmail.com>
2025-11-17 14:01:34 +0100

    fix in  base/src/hash/wyhash.ext.c

--- base/src/hash/wyhash.ext.c
+++ base/src/hash/wyhash.ext.c
@@ -21,7 +21,7 @@ B_u64 hashQ_wyhashQ_HasherD_finalize (hashQ_wyhashQ_Hasher self) {
     return result;
 }
 
-uint64_t hashQ_wyhashQ_U_hash (uint64_t seed, B_bytes data) {
+uint64_t hashQ_wyhashQ_U_1hash (uint64_t seed, B_bytes data) {
     uint64_t result = zig_hash_wyhash_hash(seed, data);
     return result;
 }
