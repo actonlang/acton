e9bf85289acac1409e87a960c02d32706218db17
Björn von Sydow <bjorn.von.sydow@gmail.com>
2026-05-29 10:46:01 +0200

    vfixed bug in.ext.c file in regression test

--- test/regression_auto/not_implemented.ext.c
+++ test/regression_auto/not_implemented.ext.c
@@ -2,7 +2,7 @@ void not_implementedQ___ext_init__() {
     // NOP
 }
 
-int64_t not_implementedQ_U_1there (B_str x) {
+int64_t not_implementedQ_there (B_str x) {
     return 0;
 }
 
@@ -13,6 +13,6 @@ $WORD not_implementedQ_generic (B_Eq W_25, $WORD x) {
     return x;
 }
 
-$R not_implementedQ_mainD_absentG_local (not_implementedQ_main __self__, $Cont c$cont, B_int i) {
-    return $R_CONT(c$cont, i);
+$R not_implementedQ_mainD_absentG_local (not_implementedQ_main __self__, $Cont c$cont, int64_t i) {
+    return $R_CONT(c$cont, toB_int(i));
 }
