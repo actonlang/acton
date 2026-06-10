800bdd987c19096463d53e929f34d633e9c3d5c7
Johan Nordlander <johan.nordlander@dataductus.se>
2026-06-10 11:24:29 +0200

    Update mapping of library module listings to C source filepaths

--- compiler/acton/Main.hs
+++ compiler/acton/Main.hs
@@ -2073,7 +2073,7 @@ explicitBuildLibraries selectedSources spec =
   where
     selectedSet = Data.Set.fromList selectedSources
     moduleCSource modName =
-      collapseDots ("out/types/" ++ map dotToSlash modName ++ ".c")
+      collapseDots ("out/types/" ++ BuildSpec.specName spec ++ "/" ++ map dotToSlash modName ++ ".c")
     dotToSlash c
       | c == '.'  = '/'
       | otherwise = c
